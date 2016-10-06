026b29cb36fbd2321aa33943b42e64f251a4120b
dehann <dehann.fourie@gmail.com>
2016-10-06 19:48:04 -0400

    improved ranges example pre vid

--- examples/RangesExample.jl
+++ examples/RangesExample.jl
@@ -9,6 +9,7 @@ using RoME, IncrementalInference
 # registerGeneralVariableTypes!(cloudGraph)
 # IncrementalInference.setCloudDataLayerAPI!()
 
+# togglePrtStbLines()
 
 N = 300
 fg = initfg()
@@ -24,7 +25,7 @@ init = 300*randn(2,N)
 v1 = addNode!(fg, "l1", init, diagm([1000.0;1000.0]), N=N, ready=0)
 
 # Two landmarks
-L1, L2 = [10.0;30], [40.0;-30]
+L1, L2, L3 = [10.0;30], [30.0;-30], [70.0;30]
 l1 = addNode!(fg, "l2", (L1')', diagm([1.0;1.0]), N=N, ready=0)
 l2 = addNode!(fg, "l3", (L2')', diagm([1.0;1.0]), N=N, ready=0)
 
@@ -35,44 +36,42 @@ pp2 = PriorPoint2D(L2, diagm([1.0;1.0]), [1.0])
 f = addFactor!(fg, [l2], pp2)
 
 # and constraints to pose x1
-rhoZ1 = norm(L1)
+P1 = [0.0;0]
+rhoZ1 = norm(L1-P1)
 ppr = Point2DPoint2DRange([rhoZ1], 2.0, [1.0])
 addFactor!(fg, [v1;l1], ppr, ready=0)
 
-rhoZ2 = norm(L2)
+rhoZ2 = norm(L2-P1)
 ppr = Point2DPoint2DRange([rhoZ2], 3.0, [1.0])
 addFactor!(fg, [v1;l2], ppr, ready=0)
 
+
+#solve
+writeGraphPdf(fg)
 tree = prepBatchTree!(fg, drawpdf=true)
 
-
 inferOverTree!(fg, tree, N=N)
-# togglePrtStbLines()
 
 drawLandms(fg,showmm=true)
 
 drawLandms(fg,showmm=true,from=1,to=1)
-
 drawLandms(fg,showmm=true,from=4)
-
 
 
 # setDBAllReady!(fg)
 
+#pose 2 in truth is at
+P2 = [50.0;0.0]
+
 # drive forward 50 units
 # v2 = addOdoFG!(fg, "l4", [50.0;0.0;0.0], odoCov, ready=0, N=N)
-ppr = Point2DPoint2DRange([40.0], 3.0, [1.0])
+ppr = Point2DPoint2DRange([norm(P1-P2)], 3.0, [1.0])
 v2 = addNode!(fg, "l4", init, diagm([3.0;3.0]), N=N, ready=0)
 addFactor!(fg, [v1;v2], ppr, ready=0)
 
 
-
-# get one more range
-rhoZ3 = norm(L2-[40;0.0])
-ppr = Point2DPoint2DRange([rhoZ3], 3.0, [1.0])
-addFactor!(fg, [v2;l2], ppr, ready=0)
-
 # solve again
+writeGraphPdf(fg)
 tree = wipeBuildNewTree!(fg)
 
 inferOverTree!(fg, tree, N=N)
@@ -80,23 +79,276 @@ inferOverTree!(fg, tree, N=N)
 drawLandms(fg,showmm=true)
 
 drawLandms(fg,showmm=true,from=4)
-# drawPosesLandms(fg,from=2,to=2)
+
+
+
+# get one range to second position
+rhoZ3 = norm(L2-P2)
+ppr = Point2DPoint2DRange([rhoZ3], 3.0, [1.0])
+addFactor!(fg, [v2;l2], ppr, ready=0)
+
+# solve again
+writeGraphPdf(fg)
+tree = wipeBuildNewTree!(fg)
+
+inferOverTree!(fg, tree, N=N)
+
+drawLandms(fg,showmm=true)
+
+drawLandms(fg,showmm=true,from=4)
+
+
 
 # setDBAllReady!(fg)
 
 
-# get one more range
-rhoZ4 = norm(L1-[40;0.0])
+# get another range
+rhoZ4 = norm(L1-P2)
 ppr = Point2DPoint2DRange([rhoZ4], 3.0, [1.0])
 addFactor!(fg, [v2;l1], ppr, ready=0)
 
 
 # solve again
+writeGraphPdf(fg)
 tree = wipeBuildNewTree!(fg, drawpdf=true)
 
 inferOverTree!(fg, tree, N=N)
 
 drawLandms(fg,showmm=true)
+
+
+
+
+# START seeing a new range signal
+ppr = Point2DPoint2DRange([norm(P2-L3)], 3.0, [1.0])
+l3 = addNode!(fg, "l5", init, diagm([3.0;3.0]), N=N, ready=0)
+addFactor!(fg, [v2;l3], ppr, ready=0)
+
+
+
+
+
+#pose 3 in truth is at
+P3 = [100.0;0.0]
+
+# drive forward 50 units
+# v2 = addOdoFG!(fg, "l4", [50.0;0.0;0.0], odoCov, ready=0, N=N)
+ppr = Point2DPoint2DRange([norm(P2-P3)], 3.0, [1.0])
+v3 = addNode!(fg, "l6", init, diagm([3.0;3.0]), N=N, ready=0)
+addFactor!(fg, [v2;v3], ppr, ready=0)
+
+
+# solve again
+writeGraphPdf(fg)
+tree = wipeBuildNewTree!(fg)
+
+inferOverTree!(fg, tree, N=N)
+
+drawLandms(fg,showmm=true)
+
+drawLandms(fg,showmm=true,from=5)
+
+
+
+
+
+
+# get one range to third position
+rhoZ5 = norm(L2-P3)
+ppr = Point2DPoint2DRange([rhoZ5], 3.0, [1.0])
+addFactor!(fg, [v3;l2], ppr, ready=0)
+
+# solve again
+# writeGraphPdf(fg)
+# tree = wipeBuildNewTree!(fg)
+
+# inferOverTree!(fg, tree, N=N)
+#
+# drawLandms(fg,showmm=true)
+#
+# drawLandms(fg,showmm=true,from=5)
+
+
+
+
+
+
+
+
+rhoZ6 = norm(L1-P3)
+ppr = Point2DPoint2DRange([rhoZ6], 3.0, [1.0])
+addFactor!(fg, [v3;l1], ppr, ready=0)
+
+
+
+
+
+rhoZ6b = norm(L3-P3)
+ppr = Point2DPoint2DRange([rhoZ6b], 3.0, [1.0])
+addFactor!(fg, [v3;l3], ppr, ready=0)
+
+
+
+
+# solve again
+writeGraphPdf(fg)
+tree = wipeBuildNewTree!(fg)
+
+inferOverTree!(fg, tree, N=N)
+
+drawLandms(fg,showmm=true)
+
+drawLandms(fg,showmm=true,from=5,to=5)
+
+
+
+
+
+
+
+
+
+
+
+#pose 4 in truth is at
+P4 = [100.0;70.0]
+
+# drive forward 50 units
+# v2 = addOdoFG!(fg, "l4", [50.0;0.0;0.0], odoCov, ready=0, N=N)
+ppr = Point2DPoint2DRange([norm(P3-P4)], 3.0, [1.0])
+v4 = addNode!(fg, "l7", init, diagm([3.0;3.0]), N=N, ready=0)
+addFactor!(fg, [v3;v4], ppr, ready=0)
+
+
+# solve again
+writeGraphPdf(fg)
+tree = wipeBuildNewTree!(fg)
+
+inferOverTree!(fg, tree, N=N)
+
+drawLandms(fg,showmm=true)
+
+drawLandms(fg,showmm=true,from=5)
+
+
+
+
+
+
+
+
+
+# get one range to fourth position
+rhoZ7 = norm(L2-P4)
+ppr = Point2DPoint2DRange([rhoZ7], 3.0, [1.0])
+addFactor!(fg, [v4;l2], ppr, ready=0)
+
+# solve again
+writeGraphPdf(fg)
+tree = wipeBuildNewTree!(fg)
+
+inferOverTree!(fg, tree, N=N)
+
+drawLandms(fg,showmm=true)
+
+drawLandms(fg,showmm=true,from=4)
+
+
+
+
+
+
+
+# get one range to fourth position
+rhoZ8 = norm(L1-P4)
+ppr = Point2DPoint2DRange([rhoZ8], 3.0, [1.0])
+addFactor!(fg, [v4;l1], ppr, ready=0)
+
+# solve again
+writeGraphPdf(fg)
+tree = wipeBuildNewTree!(fg)
+
+inferOverTree!(fg, tree, N=N)
+
+drawLandms(fg,showmm=true)
+
+drawLandms(fg,showmm=true,from=6)
+
+
+
+
+
+
+
+# get one range to fourth position
+rhoZ9 = norm(L3-P4)
+ppr = Point2DPoint2DRange([rhoZ9], 3.0, [1.0])
+addFactor!(fg, [v4;l3], ppr, ready=0)
+
+# solve again
+writeGraphPdf(fg)
+tree = wipeBuildNewTree!(fg)
+
+inferOverTree!(fg, tree, N=N)
+
+drawLandms(fg,showmm=true)
+
+drawLandms(fg,showmm=true,from=5,to=5)
+
+
+
+
+
+
+
+
+
+
+#pose 5 in truth is at
+P5 = [150.0;70.0]
+
+# drive forward 50 units
+# v2 = addOdoFG!(fg, "l4", [50.0;0.0;0.0], odoCov, ready=0, N=N)
+ppr = Point2DPoint2DRange([norm(P4-P5)], 3.0, [1.0])
+v5 = addNode!(fg, "l8", init, diagm([3.0;3.0]), N=N, ready=0)
+addFactor!(fg, [v4;v5], ppr, ready=0)
+
+
+rhoZ10 = norm(L3-P5)
+ppr = Point2DPoint2DRange([rhoZ10], 3.0, [1.0])
+addFactor!(fg, [v5;l3], ppr, ready=0)
+
+
+
+rhoZ11 = norm(L2-P5)
+ppr = Point2DPoint2DRange([rhoZ11], 3.0, [1.0])
+addFactor!(fg, [v5;l2], ppr, ready=0)
+
+
+# solve again
+writeGraphPdf(fg)
+tree = wipeBuildNewTree!(fg)
+
+inferOverTree!(fg, tree, N=N)
+
+drawLandms(fg,showmm=true)
+
+
+
+
+rhoZ12 = norm(L1-P5)
+ppr = Point2DPoint2DRange([rhoZ12], 3.0, [1.0])
+addFactor!(fg, [v5;l1], ppr, ready=0)
+
+
+# solve again
+writeGraphPdf(fg)
+tree = wipeBuildNewTree!(fg)
+
+inferOverTree!(fg, tree, N=N)
+
+drawLandms(fg,showmm=true)
+
 
 
 #
---
+++ examples/RangesExampleTwo.jl
@@ -0,0 +1,233 @@
+using RoME, IncrementalInference
+
+# using CloudGraphs, Neo4j
+# include("BlandAuthDB.jl")
+#
+# configuration = CloudGraphs.CloudGraphConfiguration(dbaddress, 7474, dbusr, dbpwd, mongoaddress, 27017, false, "", "");
+# cloudGraph = connect(configuration);
+# # register types of interest in CloudGraphs
+# registerGeneralVariableTypes!(cloudGraph)
+# IncrementalInference.setCloudDataLayerAPI!()
+
+# togglePrtStbLines()
+
+
+GTp = Dict{ASCIIString, Vector{Float64}}()
+GTl = Dict{ASCIIString, Vector{Float64}}()
+
+
+GTp["l100"] = [0.0;0]
+GTp["l101"] = [50.0;0]
+GTp["l102"] = [100.0;0]
+GTp["l103"] = [100.0;50.0]
+GTp["l104"] = [100.0;100.0]
+GTp["l105"] = [50.0;100.0]
+GTp["l106"] = [0.0;100.0]
+GTp["l107"] = [0.0;50.0]
+GTp["l108"] = [0.0;-50.0]
+GTp["l109"] = [0.0;-100.0]
+GTp["l110"] = [50.0;-100.0]
+GTp["l111"] = [100.0;-100.0]
+GTp["l112"] = [100.0;-50.0]
+
+GTl["l1"] = [10.0;30]
+GTl["l2"] = [30.0;-30]
+GTl["l3"] = [80.0;40]
+GTl["l4"] = [120.0;-50]
+
+cursor = [0.0;0]
+
+function landmsInRange(GTl::Dict{ASCIIString, Vector{Float64}}, cur::Vector{Float64};
+			lim::Float64=100.0)
+	inrange = Dict{ASCIIString, Float64}()
+	for gtl in GTl
+		rho = norm(gtl[2]-cur)
+		if rho < lim
+			inrange[gtl[1]] = rho
+		end
+	end
+	return inrange
+end
+
+function isInFG!(fgl::FactorGraph, lbl::ASCIIString; N=100, ready=0)
+	v = nothing
+	if !haskey(fgl.IDs, lbl)
+		init = 300*randn(2,N)
+		v = addNode!(fgl, lbl, init, diagm([1000.0;1000.0]), N=N, ready=ready)
+	else
+		v = getVert(fgl, lbl)
+	end
+	v
+end
+
+function addLandmsOnPose!(fgl::FactorGraph, pose::Graphs.ExVertex, GTl::Dict{ASCIIString, Float64};
+			ready=0,N=100)
+	for gtl in GTl
+		println("addLandmsOnPose! -- adding $(gtl[1])")
+		v = isInFG!(fgl, gtl[1], N=N,ready=ready)
+		# add the constraint
+		ppr = Point2DPoint2DRange([gtl[2]], 2.0, [1.0])
+		addFactor!(fgl, [pose;v], ppr, ready=ready)
+	end
+	nothing
+end
+
+function addNewPose!(fgl::FactorGraph, from::ASCIIString, lbl::ASCIIString, GTp;
+			ready=0, N=N)
+
+	init = 300*randn(2,N)
+
+	v = addNode!(fgl, lbl, init, diagm([1000.0;1000.0]), N=N, ready=ready)
+	rhoZ = norm(GTp[lbl]-GTp[from])
+	ppr = Point2DPoint2DRange([rhoZ], 3.0, [1.0])
+	f = addFactor!(fgl, [getVert(fgl, from);v], ppr, ready=ready)
+	pts = evalFactor2(fgl, f, v.index)
+	setVal!(v, pts)
+	updateFullVert!(fgl,v)
+	getVert(fgl, lbl)
+end
+
+function drive(fgl::FactorGraph, GTp, GTl, from, to; N=100)
+	v = addNewPose!(fgl, from, to, GTp, N=N)
+	# v = getVert(fgl, to)
+	addLandmsOnPose!(fgl, v, landmsInRange(GTl, GTp[to], lim=120.0), N=N )
+	writeGraphPdf(fgl)
+	nothing
+end
+
+function batchsolve(fgl::FactorGraph)
+	tree = wipeBuildNewTree!(fgl, drawpdf=true)
+	inferOverTree!(fgl, tree, N=N)
+	nothing
+end
+
+
+
+N = 300
+fg = initfg()
+fg.sessionname="SESSranges"
+# fg.cg = cloudGraph
+
+# Some starting position
+init = 300*randn(2,N)
+v1 = addNode!(fg, "l100", init, diagm([1000.0;1000.0]), N=N, ready=0)
+
+
+lmv1 = landmsInRange(GTl, GTp["l100"])
+addLandmsOnPose!(fg, v1, lmv1, N=N )
+
+# must pin landmarks for guage
+pp2 = PriorPoint2D(GTl["l1"], diagm([1.0;1.0]), [1.0])
+f = addFactor!(fg,[getVert(fg,"l1")], pp2)
+pp2 = PriorPoint2D(GTl["l2"], diagm([1.0;1.0]), [1.0])
+f = addFactor!(fg, [getVert(fg,"l2")], pp2)
+
+
+
+writeGraphPdf(fg)
+
+batchsolve(fg)
+drawLandms(fg,showmm=true)
+
+
+addNewPose!(fg, "l100", "l101", GTp, N=N)
+writeGraphPdf(fg)
+
+batchsolve(fg)
+drawLandms(fg,showmm=true)
+
+
+
+
+addLandmsOnPose!(fg, getVert(fg, "l101"),
+								landmsInRange(GTl, GTp["l101"]), N=N )
+
+writeGraphPdf(fg)
+
+batchsolve(fg)
+drawLandms(fg,showmm=true)
+
+
+
+
+drive(fg, GTp, GTl, "l101", "l102", N=N)
+
+batchsolve(fg)
+drawLandms(fg,showmm=true)
+
+
+
+
+
+drive(fg, GTp, GTl, "l102", "l103", N=N)
+
+batchsolve(fg)
+drawLandms(fg,showmm=true)
+
+
+
+
+
+
+drive(fg, GTp, GTl, "l103", "l104", N=N)
+batchsolve(fg)
+drawLandms(fg,showmm=true)
+
+
+
+
+
+drive(fg, GTp, GTl, "l104", "l105", N=N)
+batchsolve(fg)
+drawLandms(fg,showmm=true)
+
+
+
+
+
+drive(fg, GTp, GTl, "l105", "l106", N=N)
+batchsolve(fg)
+drawLandms(fg,showmm=true)
+
+
+
+
+
+drive(fg, GTp, GTl, "l106", "l107", N=N)
+batchsolve(fg)
+drawLandms(fg,showmm=true)
+
+
+
+drive(fg, GTp, GTl, "l107", "l108", N=N)
+batchsolve(fg)
+drawLandms(fg,showmm=true)
+
+
+
+drive(fg, GTp, GTl, "l108", "l109", N=N)
+batchsolve(fg)
+drawLandms(fg,showmm=true)
+
+
+
+
+drive(fg, GTp, GTl, "l109", "l110", N=N)
+batchsolve(fg)
+drawLandms(fg,showmm=true)
+
+
+
+drive(fg, GTp, GTl, "l110", "l111", N=N)
+batchsolve(fg)
+drawLandms(fg,showmm=true)
+
+
+
+drive(fg, GTp, GTl, "l111", "l112", N=N)
+batchsolve(fg)
+drawLandms(fg,showmm=true)
+
+
+
+#
