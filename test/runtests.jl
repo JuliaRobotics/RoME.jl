2e076f85863e1851cd64cc5fcae510f345a5d791
dehann <dehann.fourie@gmail.com>
2017-01-23 11:55:35 -0500

    add more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,6 +12,10 @@ println("[TEST] Camera function evaluations...")
 include("testCameraFunctions.jl")
 println("[SUCCESS]")
 
+println("[TEST] numeric root...")
+include("testhigherdimroots.jl")
+println("[SUCCESS]")
+
 println("[TEST] Linear array functions evaluations...")
 include("testDidsonFunctions.jl")
 println("[SUCCESS]")
