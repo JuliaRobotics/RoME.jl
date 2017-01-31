4b5415f2e0b11415b5a147e8fdf99806b4680ceb
dehann <dehann.fourie@gmail.com>
2017-01-31 18:15:59 -0500

    wip on drawing

--- src/MultipleFeaturesConstraint.jl
+++ src/MultipleFeaturesConstraint.jl
@@ -37,22 +37,31 @@ type MultipleFeatures2D <: IncrementalInference.FunctorPairwiseMinimize
   xir3::Normal
   xjr1::Normal
   xjr2::Normal
-  xjr3::Normal
-  bTc
+  xjr3a::Normal
+  xjr3b::Normal
+  abC::Categorical
+  bTc::Array{Float64,2}
   zDim::Tuple{Int,Int,Int,Int,Int}
   # Takes five
-  MultipleFeatures2D(a,b,c,d,e,f,g) = new(a,b,c,d,e,f,g, (3,3,2,2,2) )
+  MultipleFeatures2D(a,b,c,d,e,f,g,h,i ) = new(a,b,c,d,e,f,g,h,i, (3,3,2,2,2) )
 end
 
 function getSample(mm2d::MultipleFeatures2D, N::Int=1)
+
   meas = Array{Float64,3}(3,N,2)
   meas[1,:,1] = rand(mm2d.xir1,N)
   meas[2,:,1] = rand(mm2d.xir2,N)
   meas[3,:,1] = rand(mm2d.xir3,N)
   meas[1,:,2] = rand(mm2d.xjr1,N)
   meas[2,:,2] = rand(mm2d.xjr2,N)
-  meas[3,:,2] = rand(mm2d.xjr3,N)
-  return (meas,)
+  meas[3,:,2] = rand(mm2d.xjr3a,N)
+
+  select = rand(mm2d.abC,N)
+  beb = select .== 2
+  numb = sum(beb)
+  meas[3,beb,2] = rand(mm2d.xjr3b,numb)
+
+  return (meas, beb, numb)
 end
 
 # function (mm2d::MultipleFeatures2D)(res::Array{Float64}, idx::Int, meas::Tuple,
@@ -96,19 +105,25 @@ end
 
 
 # redo with angles on sightings for minimization
-# NOTE -- currently an inefficient implementation
+# NOTE -- an inefficient implementation
 function (mm2d::MultipleFeatures2D)(res::Array{Float64}, idx::Int, meas::Tuple,
           wAbi::Array{Float64,2},
           wAbj::Array{Float64,2},
           wAo1::Array{Float64,2},
           wAo2::Array{Float64,2},
-          wAo3::Array{Float64,2}  )
+          wAo3::Array{Float64,2},
+          wAo3b::Union{Array{Float64,2},Bool}=false  )
   #
+  len3b = wAo3b==false ? 0 : size(wAo3b,2)
+  dobimodal = len3b > 0
+  meas[3] > 0 && !dobimodal ? error("mm2d::MultipleFeatures2D -- bi-modal mismatch") : nothing
+
   wTbi = SE2(wAbi[:,idx])
   wTbj = SE2(wAbj[:,idx])
   wTo1 = SE2([wAo1[:,idx];0.0])
   wTo2 = SE2([wAo2[:,idx];0.0])
   wTo3 = SE2([wAo3[:,idx];0.0])
+  wTo3b = dobimodal ? SE2([wAo3b[:,idx];0.0]) : nothing
 
   ciTo1 = (wTbi * mm2d.bTc) \ wTo1
   ciTo2 = (wTbi * mm2d.bTc) \ wTo2
@@ -117,6 +132,7 @@ function (mm2d::MultipleFeatures2D)(res::Array{Float64}, idx::Int, meas::Tuple,
   cjTo1 = (wTbj * mm2d.bTc) \ wTo1
   cjTo2 = (wTbj * mm2d.bTc) \ wTo2
   cjTo3 = (wTbj * mm2d.bTc) \ wTo3
+  cjTo3b = dobimodal ? (wTbj * mm2d.bTc) \ wTo3 : nothing
 
   the1 = atan2(se2vee(ciTo1)[2], se2vee(ciTo1)[1])
   the2 = atan2(se2vee(ciTo2)[2], se2vee(ciTo2)[1])
@@ -130,12 +146,23 @@ function (mm2d::MultipleFeatures2D)(res::Array{Float64}, idx::Int, meas::Tuple,
   the1 = atan2(se2vee(cjTo1)[2], se2vee(cjTo1)[1])
   the2 = atan2(se2vee(cjTo2)[2], se2vee(cjTo2)[1])
   the3 = atan2(se2vee(cjTo3)[2], se2vee(cjTo3)[1])
+  the3b = dobimodal ? atan2(se2vee(cjTo3b)[2], se2vee(cjTo3b)[1]) : nothing
 
   res[2] = 0.0
   res[2] += ( meas[1][1,idx,2] - the1 )^2
   res[2] += ( meas[1][2,idx,2] - the2 )^2
-  res[2] += ( meas[1][3,idx,2] - the3 )^2
 
+  if dobimodal
+    if !meas[2][idx]
+      # 3a case
+      res[2] += ( meas[1][3,idx,2] - the3 )^2
+    else
+      # 3b case
+      res[2] += ( meas[1][3,idx,2] - the3b )^2
+    end
+  else
+    res[2] += ( meas[1][3,idx,2] - the3 )^2
+  end
   sum(res)
 end
 
--- test/testmultiplefeatures.jl
+++ test/testmultiplefeatures.jl
@@ -13,6 +13,7 @@ x2 = [1.0;-1.0;pi/2]
 l1 = [0.5;0.5]
 l2 = [1.0;0.0]
 l3 = [1.5;-0.5]
+l3b = [1.5;0.0]
 L = [l1';l2';l3']
 
 # plot(x=L[:,1],y=L[:,2],Geom.point)
@@ -34,6 +35,8 @@ the22 = atan2(tl2[2],tl2[1])
 tl3 = se2vee(Xj \ SE2([l3;0.0]))[1:2]
 the23 = atan2(tl3[2],tl3[1])
 
+tl3b = se2vee(Xj \ SE2([l3b;0.0]))[1:2]
+the23b = atan2(tl3b[2],tl3b[1])
 
 xir1 = Normal(the11,1e-4)
 xir2 = Normal(the12,1e-4)
@@ -41,7 +44,9 @@ xir3 = Normal(the13,1e-4)
 xjr1 = Normal(the21,1e-4)
 xjr2 = Normal(the22,1e-4)
 xjr3 = Normal(the23,1e-4)
+xjr3b = Normal(the23b,1e-4)
 
+# Not using second feature
 mm2 = MultipleFeatures2D(
   xir1,
   xir2,
@@ -49,6 +54,8 @@ mm2 = MultipleFeatures2D(
   xjr1,
   xjr2,
   xjr3,
+  xjr3b,
+  Categorical([1.0;0.0]),
   SE2(zeros(3))  )
 
 res = randn(3)
@@ -165,8 +172,8 @@ gg = (x,y) -> minmickey([x;y])
 N=100
 fg = initfg()
 
-initCov = 0.1*eye(3)
-initCov[3,3] = 0.01
+initCov = 0.01*eye(3)
+initCov[3,3] = 0.001
 odoCov = deepcopy(initCov)
 
 ipp = PriorPose2(zeros(3,1), initCov,[1.0])
@@ -175,8 +182,8 @@ v1 = addNode!(fg, :x1, 0.001*randn(3,N), diagm([1.0;1.0;0.1]), N=N)
 
 f1  = addFactor!(fg,[:x1], ipp)
 
+pts2 = [0.014*randn(1,N)+1;  0.014*randn(1,N)-1; 0.002*randn(1,N)+pi/2]
 
-pts2 = [0.1*randn(1,N)+1;  0.1*randn(1,N)-1; 0.01*randn(1,N)+pi/2]
 v2 = addNode!(fg, :x2, pts2, diagm([1.0;1.0;0.1]), N=N)
 
 px2 = zeros(3,1)
@@ -192,39 +199,71 @@ vl3 = addNode!(fg, :l3, rand(MvNormal(l3,0.001*eye(2)),N), diagm([1.0;1.0]), N=N
 
 f2 = addFactor!(fg, [v1;v2;vl1;vl2;vl3], mm2)
 
-ef2pts = evalFactor2(fg, f2, fg.IDs[:x2])
+ef2pts = evalFactor2(fg, f2, fg.IDs[:l2])
 
-# plotKDE(kde!(ef2pts[1:2,:]))
-# plotKDE(marginal(kde!(ef2pts),[1;2]))
+using KernelDensityEstimate
+# plotKDE(kde!(ef2pts[1,:]))
 
 tree = wipeBuildNewTree!(fg)
 # spyCliqMat(tree.cliques[1])
 
-inferOverTreeR!(fg,tree)
-#
-# using KernelDensityEstimate
+[inferOverTree!(fg,tree, N=N) for i in 1:1]
 #
 #
-# plotKDE([getVertKDE(fg, :l1);getVertKDE(fg, :l2);getVertKDE(fg, :l3)])
 #
-# plotKDE([marginal(getVertKDE(fg, :x1),[1;2]);marginal(getVertKDE(fg, :x2),[1;2]) ])
+arrp = [getVertKDE(fg, :l1);getVertKDE(fg, :l2);getVertKDE(fg, :l3)]
+# arrp1 = deepcopy(arrp)
+# plotKDE([arrp;arrp1],levels=1,c=["red";"red";"red";"blue";"blue";"blue"])
 #
-# plotKDE(marginal(getVertKDE(fg,:x2),[1;2]))
+
+arrp=[marginal(getVertKDE(fg, :x1),[1;2]);marginal(getVertKDE(fg, :x2),[1;2]) ]
+plotKDE(arrp,levels=1)
 #
+# plotKDE(marginal(getVertKDE(fg,:x1),[3]))
 #
-# @show ef2pts
+
+
+# pp,arr = localProduct(fg,:x2)
+# cc = plotKDE([pp;arr],c=["red";"black";"blue"],levels=1);
+
+cc = drawLbl(fg,:l1);
+Gadfly.draw(PDF("/home/dehann/Desktop/test.pdf",20cm,15cm),cc)
+
+
+lbl = :x2
+
+cliq = whichCliq(tree, string(lbl))
+cliqdbg = cliq.attributes["debug"]
+tmpfilepath = joinpath(dirname(@__FILE__),"tmpimgs")
+for i in 1:length(cliqdbg.mcmc)
+  ppr = kde!(cliqdbg.mcmc[i].prods[1].prev)
+  ppp = kde!(cliqdbg.mcmc[i].prods[1].product)
+  cc = plotKDE([ppr;ppr;cliqdbg.mcmc[i].prods[1].potentials],c=["black";"red";"green";"blue";"cyan";"deepskyblue"],levels=1);
+  Gadfly.draw(PNG(joinpath(tmpfilepath,"$(string(lbl))mcmc$(i).png"),20cm,15cm),cc)
+end
+run(`convert -delay 100 $(tmpfilepath)/$(string(lbl))mcmc*.png $(tmpfilepath)/$(string(lbl))mcmc.gif`)
+
+run(`eog $(tmpfilepath)/$(string(lbl))mcmc.gif`)
+
+mcp = mcmcPose2D!(plots, cliqdbg)
+
+
+println("test ambiguous bi-modal multifeature constraint operation")
 
 
 
 
 
-
-
-
-
-
-
-
+mm2 = MultipleFeatures2D(
+  xir1,
+  xir2,
+  xir3,
+  xjr1,
+  xjr2,
+  xjr3,
+  xjr3b,
+  Categorical([0.5;0.5]),
+  SE2(zeros(3))  )
 
 
 
