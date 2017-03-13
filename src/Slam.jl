0b0da3276f4ab378992ab10aca436fc75ec13a96
dehann <dehann.fourie@gmail.com>
2017-03-13 19:26:25 -0400

    wip on partials method for lcm server and predicted belief init

--- src/Slam.jl
+++ src/Slam.jl
@@ -19,18 +19,62 @@ end
 function addOdoFG!(slaml::SLAMWrapper, odo::Pose3Pose3;
                   N::Int=100, ready::Int=1,
                   saveusrid::Int=-1)
-    #
-    vprev = getVert(slaml.fg, slaml.lastposesym)
-    # vprev, X, nextn = getLastPose(fgl)
-    npnum = parse(Int,string(slaml.lastposesym)[2:end]) + 1
-    nextn = Symbol("x$(npnum)")
-    vnext = addNode!(slaml.fg, nextn, getVal(vprev) ⊕ odo, N=N, ready=ready)
-    slaml.lastposesym = nextn
-    fact = addFactor!(slaml.fg, [vprev;vnext], odo)
+  #
+  vprev = getVert(slaml.fg, slaml.lastposesym)
+  # vprev, X, nextn = getLastPose(fgl)
+  npnum = parse(Int,string(slaml.lastposesym)[2:end]) + 1
+  nextn = Symbol("x$(npnum)")
+  vnext = addNode!(slaml.fg, nextn, getVal(vprev) ⊕ odo, N=N, ready=ready)
+  slaml.lastposesym = nextn
+  fact = addFactor!(slaml.fg, [vprev;vnext], odo)
 
-    if saveusrid > -1
-      slaml.lbl2usrid[nextn] = saveusrid
-      slaml.usrid2lbl[saveusrid] = nextn
-    end
-    return vnext, fact
+  if saveusrid > -1
+    slaml.lbl2usrid[nextn] = saveusrid
+    slaml.usrid2lbl[saveusrid] = nextn
+  end
+  return vnext, fact
 end
+
+
+function addposeFG!(slaml::SLAMWrapper,
+      constrs::Vector{IncrementalInference.FunctorInferenceType};
+      N::Int=100,
+      ready::Int=1,
+      saveusrid::Int=-1   )
+  #
+  vprev = getVert(slaml.fg, slaml.lastposesym)
+
+  npnum = parse(Int,string(slaml.lastposesym)[2:end]) + 1
+  nextn = Symbol("x$(npnum)")
+  # preinit
+  vnext = addNode!(slaml.fg, nextn, getVal(vprev), N=N, ready=ready)
+  slaml.lastposesym = nextn
+
+  facts = Graphs.ExVertex[]
+  PP = BallTreeDensity[]
+  for cns = constrs
+    fa = nothing
+    if typeof(cns) == FunctorPairwise
+      fa = addFactor!(slaml.fg, [vprev;vnext], cns)
+    elseif typeof(cns) == FunctorSingleton
+      fa = addFactor!(slaml.fg, [vnext], cns)
+    else
+      error("Unrecognized type $typeof(cns)")
+    end
+    # ppts = evalFactor2(slaml.fg, fa, vnext)
+    # push!(PP, kde!(ppts))
+    push!(facts, fa)
+  end
+
+  # set node val from new constraints as init
+  val = predictbelief(slaml.fg, vnext, facts, N=N)
+
+  if saveusrid > -1
+    slaml.lbl2usrid[nextn] = saveusrid
+    slaml.usrid2lbl[saveusrid] = nextn
+  end
+  return vnext, facts
+end
+
+
+#
