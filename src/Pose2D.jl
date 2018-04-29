0354bcb5aedf8193c4a3a6f3b87b0acf721f0742
dehann <dehann.fourie@gmail.com>
2018-04-29 14:45:49 -0400

    small update in softtype

--- src/Pose2D.jl
+++ src/Pose2D.jl
@@ -1,21 +1,16 @@
 # Pose2 functions for Robot Motion Estimate
 
-# TODO - move to IncrementalInference
-struct Prior{T} <: IncrementalInference.FunctorSingleton where {T <: Distribution}
-  z::T
-end
-getSample(s::Prior, N::Int=1) = (rand(s.z,N), )
-
-
 struct Pose2 <: IncrementalInference.InferenceVariable
   dims::Int
   labels::Vector{String}
   Pose2() = new(3, String["POSE";])
 end
-struct Point2 <: IncrementalInference.InferenceVariable
-  dims::Int
-  Point2() = new(2)
+
+# TODO - move to IncrementalInference
+struct Prior{T} <: IncrementalInference.FunctorSingleton where {T <: Distribution}
+  z::T
 end
+getSample(s::Prior, N::Int=1) = (rand(s.z,N), )
 
 
 mutable struct Pose2Pose2_NEW{T} <: IncrementalInference.FunctorPairwise where {T <: Distribution}
