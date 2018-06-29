c79be1839a4f7c6d610f4825070b4605c4d18bf2
dehann <dehann.fourie@gmail.com>
2018-06-28 22:05:58 -0400

    wip, not finding rotation solution on new test yet

--- test/testhigherdimroots.jl
+++ test/testhigherdimroots.jl
@@ -1,33 +1,74 @@
-using RoME, IncrementalInference, TransformUtils
+using RoME, IncrementalInference, TransformUtils, Distributions
 using Base.Test
 
 
 println("Increased dimension root finding test")
 
+mutable struct RotationTest <: IncrementalInference.FunctorPairwise
+  z::MvNormal
+end
+
 # 3 dimensional line, z = [a b][x y]' + c
-function rotationresidual!(res::Vector{Float64}, z::Vector{Float64}, var::Tuple)
+function (rt::RotationTest)(res::Vector{Float64}, userdata, idx, meas, var1,var2)
+  z = meas[1]
   q1 = convert(Quaternion, Euler(z...))
-  q2 = convert(Quaternion, so3(var[2]))
+  s = so3(var2[:,idx])
+  q2 = convert(Quaternion, s)
   qq = q1*q_conj(q2)
-  res[1:3] = vee(convert(so3, qq))
+  vee!(res, convert(so3, qq))
   nothing
 end
 
+
+N = 1
+
 for i in 1:10
-  eul = 0.25*randn(3)
+  eul = 0.25*randn(3, N)
   # res = zeros(3)
   # @show rotationresidual!(res, eul, (zeros(0),x0))
   # @show res
-  gg = (res, x) -> rotationresidual!(res, eul, (zeros(0),x))
+  # gg = (res, x) -> rotationresidual!(res, eul, (zeros(0),x))
   x0 = 0.1*randn(3)
   res = zeros(3)
   # @show gg(res, x0)
   # @show res
-  y = numericRootGenericRandomizedFnc(
-          gg,
-          3, 3, x0   )
-  # test the result
-  @show q1 = convert(Quaternion, Euler(eul...))
-  @show q2 = convert(Quaternion, so3(y))
-  @test TransformUtils.compare(q1, q2, tol=1e-8)
+
+  A = rand(3,N)
+  B = rand(3,N)
+  At = deepcopy(A)
+  t = Array{Array{Float64,2},1}()
+  push!(t,A)
+  push!(t,B)
+  rr = RotationTest(MvNormal(zeros(3), 0.001*eye(3)))
+
+  gwp = GenericWrapParam{RotationTest}(rr, t, 1, 1)
+
+  @time gwp(res, x0)
+
+  # gwp.activehypo
+  # gwp.hypotheses
+  # gwp.params
+
+  @show gwp.varidx
+  gwp.measurement = (eul, )
+  @show zDim = 3
+  fr = FastRootGenericWrapParam{RotationTest}(gwp.params[gwp.varidx], zDim, gwp)
+
+  @test fr.xDim == 3
+
+  # and return complete fr/gwp
+  @time for gwp.particleidx in 1:N
+    # gwp(x, res)
+    numericRootGenericRandomizedFnc!( fr )
+
+    # test the result
+    @show q1 = convert(Quaternion, Euler(eul[gwp.particleidx]...))
+    @show q2 = convert(Quaternion, so3(fr.Y))
+    @test TransformUtils.compare(q1, q2, tol=1e-8)
+  end
+
+  # y = numericRootGenericRandomizedFnc(
+  #         gg,
+  #         3, 3, x0   )
+
 end
