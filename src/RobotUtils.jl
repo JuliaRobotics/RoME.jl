4218345e3aa01821f16abe9a006597b10cdf7193
dehann <dehann.fourie@gmail.com>
2018-07-17 21:58:26 -0400

    adjusting test tolerance

--- src/RobotUtils.jl
+++ src/RobotUtils.jl
@@ -171,7 +171,6 @@ end
 Create a new variable node and insert odometry constraint factor between
 which will automatically increment latest pose symbol x<k+1> for new node new node and
 constraint factor are returned as a tuple.
-
 """
 function addOdoFG!(
         fg::FactorGraph,
