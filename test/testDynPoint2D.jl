742be5b914f10a215fe3cce8549c074499a9a4ab
dehann <dehann.fourie@gmail.com>
2018-07-25 16:03:01 -0400

    new test for velpoint

--- test/testDynPoint2D.jl
+++ test/testDynPoint2D.jl
@@ -1,6 +1,6 @@
 # test the DynPoint2 functionality
 
-using RoME, Distributions
+using RoME, IncrementalInference, Distributions
 using Base: Test
 
 
@@ -90,6 +90,57 @@ inferOverTree!(fg, tree)
 
 
 
+
+
+
+@testset "test VelPoint2VelPoint2" begin
+
+
+N=100
+pμ = [0.0,0,1,0]
+pσ = diagm([0.1;0.1;0.1;0.1].^2)
+
+fg = initfg();
+
+addNode!(fg, :x1, DynPoint2(ut=0))
+pp = DynPoint2VelocityPrior(MvNormal(pμ,pσ))
+addFactor!(fg, [:x1;], pp, autoinit=false)
+
+addNode!(fg, :x2, DynPoint2(ut=1_000_000))
+dpμ = [1.0;0;0;0];
+dpσ = diagm([1.0;1;0.5;0.01].^2)
+vp = VelPoint2VelPoint2(MvNormal(dpμ,dpσ))
+addFactor!(fg, [:x1,:x2], vp, autoinit=false)
+
+# writeGraphPdf(fg)
+
+# note, nothign is initialized yet...
+isInitialized(fg, :x1)
+isInitialized(fg, :x2)
+
+# lets init the first
+doautoinit!(fg, :x1)
+
+X1 = getVal(fg, :x1)
+@test 0.7*N < sum(-0.5 .< X1[1,:] .< 0.5)
+@test 0.7*N < sum(-0.5 .< X1[2,:] .< 0.5)
+
+# now test "projection" / probabilistic-convolution through VelPointVelPoint{D, N}
+pX2 = approxConv(fg, :x1x2f1, :x2)
+
+@test 0.7*N < sum(0 .< pX2[1,:] .< 2)
+@test 0.7*N < sum(-1 .< pX2[2,:] .< 1)
+
+
+# using RoMEPlotting, Gadfly
+# using KernelDensityEstimate
+# using KernelDensityEstimatePlotting
+# X1 = getVal(fg,:x1)
+# plot(x=pX2[1,:], y=pX2[2,:], Geom.hexbin)
+# pl = plotKDE(kde!(pX2[1:2,:]), dims=[1;2], levels=3, c=["blue"])
+# pl = plotKDE(kde!(X1[1:2,:]), dims=[1;2], levels=3, c=["blue"])
+
+end
 
 
 
