207ac5c9cd1d9b28ff0efa5162b459392460b5b9
dehann <dehann.fourie@gmail.com>
2018-09-03 00:05:56 -0400

    fixed lag factor graph solving and test

--- test/runtests.jl
+++ test/runtests.jl
@@ -38,6 +38,8 @@ include("testPoint2Point2WorldBearing.jl")
 
 include("testBearingRange2D.jl")
 
+include("testFixedLagFG.jl")
+
 include("testMultimodalRangeBearing.jl")
 
 include("testDynPose2D.jl")
