7ebf282b2e025af695fdfb6c22cd559764fd1df2
dehann <dehann.fourie@gmail.com>
2018-09-09 21:17:42 -0400

    better testing

--- test/testDynPoint2D.jl
+++ test/testDynPoint2D.jl
@@ -2,7 +2,12 @@
 
 using RoME, IncrementalInference, Distributions
 using Base: Test
+# using RoMEPlotting,
+using KernelDensityEstimate #, KernelDensityEstimatePlotting
 
+
+
+@testset "test DynPoint2..." begin
 
 # N = 75
 fg = initfg()
@@ -15,7 +20,6 @@ v1 = addNode!(fg, :x1, DynPoint2(ut=1000_000))
 # Prior factor as boundary condition
 pp0 = DynPoint2VelocityPrior(MvNormal([zeros(2);10*ones(2)], 0.1*eye(4)))
 f0 = addFactor!(fg, [:x0;], pp0)
-
 # conditional likelihood between Dynamic Point2
 dp2dp2 = DynPoint2DynPoint2(MvNormal([10*ones(2);zeros(2)], 0.1*eye(4)))
 f1 = addFactor!(fg, [:x0;:x1], dp2dp2)
@@ -24,24 +28,29 @@ f1 = addFactor!(fg, [:x0;:x1], dp2dp2)
 ensureAllInitialized!(fg)
 
 
-
-
 tree = wipeBuildNewTree!(fg)
 inferOverTree!(fg, tree)
 
 
 
-# using RoMEPlotting,
-using KernelDensityEstimate #, KernelDensityEstimatePlotting
-
 # X1 = getVal(fg, :x1)
 @show x0 = getKDEMax(getVertKDE(fg, :x0))
 
+@test norm(x0[1:2] - [0.0;0.0]) < 1.0
+@test norm(x0[3:4] - [10.0;10.0]) < 1.0
+
+
 @show x1 = getKDEMax(getVertKDE(fg, :x1))
 
+@test norm(x1[1:2] - [20.0;20.0]) < 1.0
+@test norm(x1[3:4] - [10.0;10.0]) < 1.0
+
+
+end
 
 
 
+@testset "test DynPoint2VelocityPrior..." begin
 
 # N = 75
 fg = initfg()
@@ -68,7 +77,9 @@ f2 = addFactor!(fg, [:x1;:x2], dp2dp2)
 # Graphs.plot(fg.g)
 ensureAllInitialized!(fg)
 
-
+@test isInitialized(fg, :x0)
+@test isInitialized(fg, :x1)
+@test isInitialized(fg, :x2)
 
 
 tree = wipeBuildNewTree!(fg)
@@ -78,15 +89,19 @@ inferOverTree!(fg, tree)
 
 # X1 = getVal(fg, :x1)
 @show x0 = getKDEMax(getVertKDE(fg, :x0))
+@test norm(x0[1:2] - [0.0;0.0]) < 1.0
+@test norm(x0[3:4] - [10.0;10.0]) < 1.0
 
 @show x1 = getKDEMax(getVertKDE(fg, :x1))
+@test norm(x1[1:2] - [10.0;10.0]) < 1.0
+@test norm(x1[3:4] - [10.0;10.0]) < 1.0
 
 @show x2 = getKDEMax(getVertKDE(fg, :x2))
+@test norm(x2[1:2] - [20.0;20.0]) < 1.0
+@test norm(x2[3:4] - [10.0;10.0]) < 1.0
 
 
-
-
-
+end
 
 
 
@@ -97,7 +112,7 @@ inferOverTree!(fg, tree)
 
 
 N=100
-pμ = [0.0,0,1,0]
+pμ = [0.0,0,10.0,0]
 pσ = diagm([0.1;0.1;0.1;0.1].^2)
 
 fg = initfg();
@@ -109,7 +124,7 @@ addFactor!(fg, [:x1;], pp, autoinit=false)
 ensureAllInitialized!(fg)
 
 addNode!(fg, :x2, DynPoint2(ut=1_000_000))
-dpμ = [1.0;0;0;0];
+dpμ = [10.0;0;0;0];
 dpσ = diagm([1.0;1;0.5;0.01].^2)
 vp = VelPoint2VelPoint2(MvNormal(dpμ,dpσ))
 addFactor!(fg, [:x1,:x2], vp, autoinit=false)
@@ -126,13 +141,16 @@ addFactor!(fg, [:x1,:x2], vp, autoinit=false)
 X1 = getVal(fg, :x1)
 @test 0.7*N < sum(-0.5 .< X1[1,:] .< 0.5)
 @test 0.7*N < sum(-0.5 .< X1[2,:] .< 0.5)
+@test 0.7*N < sum(9.0 .< X1[3,:] .< 11.0)
+@test 0.7*N < sum(9.0 .< X1[3,:] .< 11.0)
 
 # now test "projection" / probabilistic-convolution through VelPointVelPoint{D, N}
 pX2 = approxConv(fg, :x1x2f1, :x2)
 
-@test 0.7*N < sum(0 .< pX2[1,:] .< 2)
-@test 0.7*N < sum(-1 .< pX2[2,:] .< 1)
-
+@test 0.7*N < sum(9 .< pX2[1,:] .< 11)
+@test 0.7*N < sum(-2 .< pX2[2,:] .< 2)
+@test 0.7*N < sum(9.0 .< pX2[3,:] .< 11.0)
+@test 0.7*N < sum(9.0 .< pX2[3,:] .< 11.0)
 
 # using RoMEPlotting, Gadfly
 # using KernelDensityEstimate
