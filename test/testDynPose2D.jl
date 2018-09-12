ca4811acff257687d3f0fcc0d2cc1afa730f69e3
dehann <dehann.fourie@gmail.com>
2018-09-12 09:14:04 -0400

    test tolerance

--- test/testDynPose2D.jl
+++ test/testDynPose2D.jl
@@ -238,7 +238,7 @@ batchSolve!(fg)
 # test for velocity in the body frame
 x0 = KDE.getKDEMean(getVertKDE(fg, :x0))
 
-@test 0.0 < x0[1] < 2.0
+@test -0.4 < x0[1] < 2.0
 @test abs(x0[2]) < 0.5
 @test abs(x0[3] - pi/2) < 0.1
 @test abs(x0[4]) < 0.4
