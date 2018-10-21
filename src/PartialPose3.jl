d460a86ec6b255fe4c223031365f0e8e48482097
gearsad <sclaassens.ad@gmail.com>
2018-10-20 22:47:10 -0500

    Working session building out converters

--- src/PartialPose3.jl
+++ src/PartialPose3.jl
@@ -1,44 +1,64 @@
 # Partial Pose3 constraints
 
 
-mutable struct PartialPriorRollPitchZ <: IncrementalInference.FunctorSingleton
-  rp::Distributions.MvNormal
-  z::Distributions.Normal
+mutable struct PartialPriorRollPitchZ{T1,T2} <: IncrementalInference.FunctorSingleton where {T1 <: SamplableBelief, T2 <: SamplableBelief}
+  rp::T1
+  z::T2
   partial::Tuple
-  PartialPriorRollPitchZ() = new()
-  PartialPriorRollPitchZ(rp::MvNormal,z::Normal) = new(rp, z, (3,4,5))
+  PartialPriorRollPitchZ{T1,T2}() = new{T1,T2}()
+  PartialPriorRollPitchZ{T1,T2}(rp::T1,z::T2) = new{T1,T2}(rp, z, (3,4,5))
   # PartialPriorRollPitchZ(x1,x2,x3,x4) = new(MvNormal(x1,x2), Normal(x3,x4), (3,4,5))
   # PartialPriorRollPitchZ(rpz::PackedPartialPriorRollPitchZ) = new(MvNormal(d.rpmu, reshapeVec2Mat(d.rpsig,2)),
                                                     # Normal(rpz.zmu, rpz.zsig) )
 end
+PartialPriorRollPitchZ(rp::T1,z::T2) where {T1 <: SamplableBelief, T2 <: SamplableBelief} = PartialPriorRollPitchZ{T1,T2}(rp, z)
 function getSample(pprz::PartialPriorRollPitchZ, N::Int=1)
-  return ([rand(pprz.z,N)';rand(pprz.rp,N)], )
+  return ([rand(pprz.z,N);rand(pprz.rp,N)'], )
 end
 
 mutable struct PackedPartialPriorRollPitchZ <: IncrementalInference.PackedInferenceType
-  rpmu::Vector{Float64}
-  rpsig::Vector{Float64}
-  zmu::Float64
-  zsig::Float64
+  rpdata::String
+  zdata::String
   PackedPartialPriorRollPitchZ() = new()
-  PackedPartialPriorRollPitchZ(x1,x2,x3,x4) = new(x1,x2,x3,x4)
-  PackedPartialPriorRollPitchZ(rp::MvNormal,z::Normal) = new(rp.μ, rp.Σ.mat[:], z.μ, z.σ)
-  # PackedPartialPriorRollPitchZ(rpz::PartialPriorRollPitchZ) = new(rpz.rp.μ, rpz.rp.Σ.mat[:], rpz.z.μ, rpz.z.σ)
+  PackedPartialPriorRollPitchZ(x1::AS,x2::AS) where {AS <:AbstractString} = new(x1,x2)
 end
 function convert(::Type{PartialPriorRollPitchZ}, d::PackedPartialPriorRollPitchZ)
-  PartialPriorRollPitchZ( MvNormal(d.rpmu, reshapeVec2Mat(d.rpsig,2)),Normal(d.zmu, d.zsig)  )
+  # TODO: Change out for extractdistributionJson
+  PartialPriorRollPitchZ( extractdistribution(d.rpdata), extractdistribution(d.zdata)  )
 end
 function convert(::Type{PackedPartialPriorRollPitchZ}, d::PartialPriorRollPitchZ)
-  PackedPartialPriorRollPitchZ( d.rp, d.z )
+  PackedPartialPriorRollPitchZ( string(d.rp), string(d.z) )
 end
 
+"""
+Converter: PartialPriorRollPitchZ::Dict{String, Any} -> PartialPriorRollPitchZ
+"""
+function convert(::Type{RoME.PartialPriorRollPitchZ}, fact::Dict{String, Any})
+    rp = fact["measurement"][1]
+    z = fact["measurement"][2]
+    rp = convert(_evalType(rp["distType"]), rp)
+    z = convert(_evalType(z["distType"]), z)
+    return PartialPriorRollPitchZ(rp, z)
+end
+
+"""
+Converter: PartialPriorRollPitchZ::Dict{String, Any} -> PartialPriorRollPitchZ
+"""
+function convert(::Type{Dict{String, Any}}, fact::RoME.PartialPriorRollPitchZ)
+    pf = Dict{String, Any}(
+        "measurement" => [
+            convert(Dict{String, Any}, fact.rp),
+            convert(Dict{String, Any}, fact.z)
+        ],
+        "factorType" => "PartialPriorRollPitchZ"
+    )
+    return pf
+end
 
 function compare(a::PartialPriorRollPitchZ, b::PartialPriorRollPitchZ; tol::Float64=1e-10)
   TP = true
-  TP = TP && norm(a.rp.μ-b.rp.μ) < tol
-  TP = TP && norm(a.rp.Σ.mat[:]-b.rp.Σ.mat[:]) < tol
-  TP = TP && norm(a.z.μ-b.z.μ) < tol
-  TP = TP && norm(a.z.σ-b.z.σ) < tol
+  TP = TP && compare(a.rp, b.rp) < tol
+  TP = TP && compare(a.z, b.z) < tol
   TP = TP && norm(collect(a.partial)-collect(b.partial)) < tol
   return TP
 end
@@ -50,14 +70,16 @@ end
 # Partial pairwise constraint between poses X,Y,Yaw
 # ------------------------------------------------------------------------------
 
-mutable struct PartialPose3XYYaw <: FunctorPairwise #RoME.BetweenPoses
-  xyy::Distributions.MvNormal
+mutable struct PartialPose3XYYaw{T1,T2} <: FunctorPairwise where {T1 <: SamplableBelief, T2 <: SamplableBelief}
+  xy::T1
+  yaw::T2
   partial::Tuple
-  PartialPose3XYYaw() = new()
-  PartialPose3XYYaw(xyy::MvNormal) = new(xyy, (1,2,6))
+  PartialPose3XYYaw{T1,T2}() = new()
+  PartialPose3XYYaw{T1,T2}(xy::T1, yaw::T2) = new(xy, yaw, (1,2,6))
 end
+PartialPose3XYYaw(xy::T1, yaw::T2) where {T1 <: SamplableBelief, T2 <: SamplableBelief} =  PartialPose3XYYaw{T1,T2}(xy, yaw)
 function getSample(pxyy::PartialPose3XYYaw, N::Int=1)
-  return (rand(pxyy.xyy,N), )
+  return ([rand(pxyy.xy,N);rand(pxyy.yaw,N)'], )
 end
 function (pxyy::PartialPose3XYYaw)(res::Array{Float64},
             userdata,
@@ -74,19 +96,43 @@ end
 
 
 mutable struct PackedPartialPose3XYYaw <: IncrementalInference.PackedInferenceType
-  vecZij::Array{Float64,1} # 3translations, 3rotation
-  vecCov::Array{Float64,1}
+  xydata::String
+  yawdata::String
   PackedPartialPose3XYYaw() = new()
-  PackedPartialPose3XYYaw(x1::Vector{Float64}, x2::Array{Float64}) = new(x1, x2[:])
+  PackedPartialPose3XYYaw(xy::Vector{Float64}, yaw::Array{Float64}) = new(x1, x2[:])
 end
 function convert(::Type{PartialPose3XYYaw}, d::PackedPartialPose3XYYaw)
-  return PartialPose3XYYaw( Distributions.MvNormal(d.vecZij,
-               reshapeVec2Mat(d.vecCov, 3))  )
+  return PartialPose3XYYaw( extractdistribution(d.xydata), extractdistribution(d.yawdata) )
 end
 function convert(::Type{PackedPartialPose3XYYaw}, d::PartialPose3XYYaw)
-  return PackedPartialPose3XYYaw(d.xyy.μ, d.xyy.Σ.mat )
+  return PackedPartialPose3XYYaw( d.xy, d.yaw )
 end
 
+"""
+Converter: Dict{String, Any} -> PartialPose3XYYaw
+"""
+
+function convert(::Type{Dict{String, Any}}, fact::RoME.PartialPose3XYYaw)
+    pf = Dict{String, Any}(
+        "measurement" => [
+            convert(Dict{String, Any}, fact.xy),
+            convert(Dict{String, Any}, fact.yaw)
+        ],
+        "factorType" => "PartialPose3XYYaw"
+    )
+    return pf
+end
+
+"""
+Converter: PartialPose3XYYaw -> Dict{String, Any}
+"""
+function convert(::Type{RoME.PartialPose3XYYaw}, fact::Dict{String, Any})
+    xy = fact["measurement"][1]
+    yaw = fact["measurement"][2]
+    xy = convert(_evalType(xy["distType"]), xy)
+    yaw = convert(_evalType(yaw["distType"]), yaw)
+    return PartialPriorRollPitchZ(xy, yaw)
+end
 
 
 function compare(a::PartialPose3XYYaw, b::PartialPose3XYYaw; tol::Float64=1e-10)
