f2157709f002421a1c5502c408d8dc6b27619ac1
dehann <dehann.fourie@gmail.com>
2019-02-19 02:29:59 -0500

    testing for fmcmc iteration order

--- test/testBasicPose2Stationary.jl
+++ test/testBasicPose2Stationary.jl
@@ -3,7 +3,7 @@
 using RoME
 using Test
 
-@testset "basic that pose2 pushed back to zero in trivial case solve..." begin
+@testset "basic pose2 trivial case without forcing autoinit..." begin
 
 fg = initfg()
 
@@ -42,11 +42,61 @@ end
 end
 
 
+
+
+
+
+
+@testset "basic pose2 with forcing bad initialization..." begin
+
+fg = initfg()
+
+cov = Matrix(Diagonal(1e-4.*[1;1;1]))
+
+# first pose position
+addVariable!(fg, :x0, Pose2)
+addFactor!(fg, [:x0], Prior(MvNormal(zeros(3),cov)))
+
+# second Pose position with odo
+addVariable!(fg, :x1, Pose2)
+addFactor!(fg, [:x0;:x1], Pose2Pose2(MvNormal(zeros(3),cov)))
+
+# third pose position with odo
+addVariable!(fg, :x2, Pose2)
+addFactor!(fg, [:x1;:x2], Pose2Pose2(MvNormal(zeros(3),cov)))
+
+ensureAllInitialized!(fg)
+
+
+badval = 0.000001.*randn(3,100)
+badval[1,:] .-= 5.0
+badval[2,:] .-= 2.0
+badval[3,:] .+= 0.5
+setValKDE!(fg, :x2, kde!(badval))
+
+tree = wipeBuildNewTree!(fg, drawpdf=true, show=true)
+
+N = 100
+batchSolve!(fg, N=N)
+
+
+
+for xx in [:x0; :x1; :x2]
+  pts = getVal(fg, xx)
+  @test 0.95*N < sum( -0.5 .< pts[1,:] .< 0.5 )
+  @test 0.95*N < sum( -0.5 .< pts[2,:] .< 0.5 )
+  @test 0.95*N < sum( -0.5 .< pts[3,:] .< 0.5 )
+end
+
+end
+
+
+
 # using RoMEPlotting
 #
 # drawPoses(fg, spscale=0.5)
-
-
+#
+# spyCliqMat(tree, :x2)
 
 
 
