ccdbe72a07ac3f13174343515604cee19436c54c
dehann <dehann.fourie@gmail.com>
2019-04-24 18:26:00 -0400

    manual init examples

---
+++ examples/Hexagonal2D_TreeInitDownChainManual.jl
@@ -0,0 +1,344 @@
+
+# using Revise
+
+##
+
+# for drawing Bayes tree with images (see debug tricks below)
+# using Cairo, Fontconfig
+# using Gadfly
+
+using RoME
+
+#  Do some plotting
+# using RoMEPlotting
+
+
+## start with an empty factor graph object
+fg = initfg()
+
+# Add the first pose :x0
+addVariable!(fg, :x0, Pose2)
+
+# Add at a fixed location PriorPose2 to pin :x0 to a starting location (10,10, pi/4)
+addFactor!(fg, [:x0], PriorPose2( MvNormal([0.0; 0.0; 0.0],
+                                           Matrix(Diagonal([0.1;0.1;0.05].^2))) ), autoinit=false )
+
+# Drive around in a hexagon
+for i in 0:5
+    psym = Symbol("x$i")
+    nsym = Symbol("x$(i+1)")
+    addVariable!(fg, nsym, Pose2)
+    pp = Pose2Pose2(MvNormal([10.0;0;pi/3], Matrix(Diagonal([0.1;0.1;0.1].^2))))
+    addFactor!(fg, [psym;nsym], pp, autoinit=false )
+end
+
+# Add landmarks with Bearing range measurements
+addVariable!(fg, :l1, Point2, labels=["LANDMARK"])
+p2br = Pose2Point2BearingRange(Normal(0,0.03),Normal(20.0,0.5))
+addFactor!(fg, [:x0; :l1], p2br, autoinit=false )
+
+
+# Add landmarks with Bearing range measurements
+p2br2 = Pose2Point2BearingRange(Normal(0,0.03),Normal(20.0,0.5))
+addFactor!(fg, [:x6; :l1], p2br2, autoinit=false )
+
+
+
+# Drive around in a hexagon
+for i in 6:11
+    psym = Symbol("x$i")
+    nsym = Symbol("x$(i+1)")
+    addVariable!(fg, nsym, Pose2)
+    pp = Pose2Pose2(MvNormal([10.0;0;-pi/3], Matrix(Diagonal([0.1;0.1;0.1].^2))))
+    addFactor!(fg, [psym;nsym], pp, autoinit=false )
+end
+
+
+
+# Add landmarks with Bearing range measurements
+p2br2 = Pose2Point2BearingRange(Normal(0,0.03),Normal(20.0,0.5))
+addFactor!(fg, [:x12; :l1], p2br2, autoinit=false )
+
+
+
+
+# tree = wipeBuildNewTree!(fg, drawpdf=true, show=true, imgs=false)
+# at = initInferTreeUp!(fg, tree, drawtree=true)
+#
+#
+# ett = ExploreTreeType(fg, tree, tree.cliques[1], nothing, NBPMessage[])
+# # downMsgPassingIterative!(ett,N=100, dbg=false, drawpdf=true);
+# downMsgPassingRecursive(ett,N=100, dbg=false, drawpdf=true);
+
+
+
+
+## drive a little more
+
+# Drive around in a hexagon
+for i in 12:17
+    psym = Symbol("x$i")
+    nsym = Symbol("x$(i+1)")
+    addVariable!(fg, nsym, Pose2)
+    pp = Pose2Pose2(MvNormal([10.0;0;pi/3], Matrix(Diagonal([0.1;0.1;0.1].^2))))
+    addFactor!(fg, [psym;nsym], pp, autoinit=false )
+end
+
+
+# Add landmarks with Bearing range measurements
+p2br2 = Pose2Point2BearingRange(Normal(0,0.03),Normal(20.0,0.5))
+addFactor!(fg, [:x18; :l1], p2br2, autoinit=false )
+
+
+
+
+tree = wipeBuildNewTree!(fg, drawpdf=true, show=true, imgs=false)
+
+
+
+## Manually organized a non-async initialization sequence
+
+docliqs = [
+14;
+13;
+12;
+11;
+4;
+9;
+3;
+10;
+8;
+7;
+6; # with async
+5; # with async
+2;
+1
+]
+
+
+
+
+
+# resetTreeCliquesForUpSolve!(tree)
+
+# queue all the tasks
+# alltasks = Vector{Task}(undef, length(treel.cliques))
+# @sync begin
+# i = docliqs[8]
+
+for i in docliqs[1:7]
+  # alltasks[i] = @async begin
+cliq = tree.cliques[i]
+clst = cliqInitSolveUp!(fg, tree, cliq, drawtree=true )
+if !(clst in [:upsolved; :downsolved; :marginalized])
+  error("Clique $(cliq.index), initInferTreeUp! -- cliqInitSolveUp! did not arrive at the desired solution statu: $clst")
+end
+  # end
+end # for
+# end
+
+
+
+alltasks = Vector{Task}(undef, length(tree.cliques))
+# @sync begin
+for i in docliqs[8:12]
+  alltasks[i] = @async begin
+    cliq = tree.cliques[i]
+    clst = cliqInitSolveUp!(fg, tree, cliq, drawtree=true, limititers=10 )
+    # if !(clst in [:upsolved; :downsolved; :marginalized])
+    #   error("Clique $(cliq.index), initInferTreeUp! -- cliqInitSolveUp! did not arrive at the desired solution statu: $clst")
+    # end
+  end
+end # for
+# end
+#
+# alltasks
+
+
+i = docliqs[13]
+cliq = tree.cliques[i]
+clst = cliqInitSolveUp!(fg, tree, cliq, drawtree=true,  limititers=1)
+0
+# if !(clst in [:upsolved; :downsolved; :marginalized])
+#   error("Clique $(cliq.index), initInferTreeUp! -- cliqInitSolveUp! did not arrive at the desired solution statu: $clst")
+# end
+
+
+# break open the cliqInitSolveUp! loop for 13
+
+
+# fg_bu = deepcopy(fg)
+# tree_bu = deepcopy(tree)
+# drawTree(tree_bu)
+# drawTree(tree)
+
+
+ts7 = @async begin
+
+cliq = tree.cliques[7]
+cliq.attributes["label"]
+getCliqStatus(cliq)
+clst = cliqInitSolveUp!(fg, tree, cliq, drawtree=true, limititers=1 )
+
+end
+
+
+ts8 = @async begin
+
+cliq = tree.cliques[8]
+cliq.attributes["label"]
+getCliqStatus(cliq)
+clst = cliqInitSolveUp!(fg, tree, cliq, drawtree=true, limititers=1 )
+
+end
+
+
+# why are the arrays empty??
+getData(cliq).upInitMsgs
+
+
+
+cliq = tree.cliques[10]
+cliq.attributes["label"]
+getCliqStatus(cliq)
+clst = cliqInitSolveUp!(fg, tree, cliq, drawtree=true, limititers=1 )
+
+
+cliq = tree.cliques[6]
+cliq.attributes["label"]
+getCliqStatus(cliq)
+clst = cliqInitSolveUp!(fg, tree, cliq, drawtree=true, limititers=1 )
+
+
+cliq = tree.cliques[5]
+cliq.attributes["label"]
+getCliqStatus(cliq)
+clst = cliqInitSolveUp!(fg, tree, cliq, drawtree=true, limititers=1 )
+
+
+cliq = tree.cliques[2]
+cliq.attributes["label"]
+getCliqStatus(cliq)
+# isready(getData(cliq).initDownChannel)
+clst = cliqInitSolveUp!(fg, tree, cliq, drawtree=true, limititers=1 )
+
+
+
+cliq = tree.cliques[1]
+cliq.attributes["label"]
+getCliqStatus(cliq)
+# isready(getData(cliq).initDownChannel)
+clst = cliqInitSolveUp!(fg, tree, cliq, drawtree=true, limititers=1 )
+
+
+
+
+
+
+
+
+
+
+0
+
+
+
+
+
+
+
+drawTree(tree)
+
+
+
+
+
+
+
+
+
+
+
+
+at = initInferTreeUp!(fg, tree, drawtree=true, limititers=100)
+
+
+
+
+ett = ExploreTreeType(fg, tree, tree.cliques[1], nothing, NBPMessage[])
+# downMsgPassingIterative!(ett,N=100, dbg=false, drawpdf=true);
+downMsgPassingRecursive(ett,N=100, dbg=false, drawpdf=true);
+
+
+
+
+
+
+
+# Initialize :l1 numerical values but do not rerun solver
+# ensureAllInitialized!(fg)
+# pl = drawPosesLandms(fg, meanmax=:max)
+# Gadfly.draw(Gadfly.PDF("/tmp/test2.pdf"),pl)  # or PNG(...)
+# @async run(`evince /tmp/test2.pdf`)
+
+
+
+#
+# import IncrementalInference: prepCliqInitMsgsDown!
+#
+#
+# function prepCliqInitMsgsDown!(fgl::FactorGraph, tree::BayesTree, cliq::Graphs.ExVertex)
+#   #
+#   @info "Clique $(cliq.index), prepCliqInitMsgsDown!"
+#   # get the current messages stored in the parent
+#   currmsgs = getCliqInitUpMsgs(cliq)
+#   @info "Clique $(cliq.index), msg keys=$(collect(keys(currmsgs)))"
+#
+#   # check if any msgs should be multiplied together for the same variable
+#   msgspervar = Dict{Symbol, Vector{BallTreeDensity}}()
+#   for (cliqid, msgs) in currmsgs
+#     @show cliqid, length(msgs)
+#     for (msgsym, msg) in msgs
+#       if !haskey(msgspervar, msgsym)
+#         msgspervar[msgsym] = Vector{BallTreeDensity}()
+#       end
+#       push!(msgspervar[msgsym], msg)
+#     end
+#   end
+#
+#   @info "Clique $(cliq.index), keys with msgs=$(collect(keys(msgspervar)))"
+#
+#   # use default allocated dict
+#   products = getData(cliq).downInitMsg # Dict{Symbol, BallTreeDensity}()
+#   # multiply multiple messages together
+#   for (msgsym, msgs) in msgspervar
+#     # check if this particular down message requires msgsym
+#     if haskey(fgl.IDs, msgsym)
+#       if length(msgspervar[msgsym]) > 1
+#         products[msgsym] = manifoldProduct(msgs, getManifolds(fgl, msgsym))
+#       else
+#         products[msgsym] = msgs[1]
+#       end
+#     else
+#       # not required, therefore remove from message to avoid confusion
+#       if haskey(products, msgsym)
+#         delete!(products, msgsym)
+#       end
+#     end
+#   end
+#
+#   @info "Clique $(cliq.index), product keys=$(collect(keys(products)))"
+#   return products
+# end
+
+
+using RoMEPlotting
+
+
+pl = drawPosesLandms(fg, meanmax=:max)
+Gadfly.draw(Gadfly.PDF("/tmp/test2.pdf"),pl)  # or PNG(...)
+@async run(`evince /tmp/test2.pdf`)
+
+
+
+#
--- examples/Hexagonal2D_TreeInitDownChainManualSmaller.jl
+++ examples/Hexagonal2D_TreeInitDownChainManualSmaller.jl
@@ -44,55 +44,105 @@ addFactor!(fg, [:x6; :l1], p2br2, autoinit=false )
 
 
 
-# Drive around in a hexagon
-for i in 6:11
-    psym = Symbol("x$i")
-    nsym = Symbol("x$(i+1)")
-    addVariable!(fg, nsym, Pose2)
-    pp = Pose2Pose2(MvNormal([10.0;0;-pi/3], Matrix(Diagonal([0.1;0.1;0.1].^2))))
-    addFactor!(fg, [psym;nsym], pp, autoinit=false )
-end
-
-
-
-# Add landmarks with Bearing range measurements
-p2br2 = Pose2Point2BearingRange(Normal(0,0.03),Normal(20.0,0.5))
-addFactor!(fg, [:x12; :l1], p2br2, autoinit=false )
-
-
-
-
-# tree = wipeBuildNewTree!(fg, drawpdf=true, show=true, imgs=false)
-# at = initInferTreeUp!(fg, tree, drawtree=true)
-#
-#
-# ett = ExploreTreeType(fg, tree, tree.cliques[1], nothing, NBPMessage[])
-# # downMsgPassingIterative!(ett,N=100, dbg=false, drawpdf=true);
-# downMsgPassingRecursive(ett,N=100, dbg=false, drawpdf=true);
-
-
-
-
-## drive a little more
-
-# Drive around in a hexagon
-for i in 12:17
-    psym = Symbol("x$i")
-    nsym = Symbol("x$(i+1)")
-    addVariable!(fg, nsym, Pose2)
-    pp = Pose2Pose2(MvNormal([10.0;0;pi/3], Matrix(Diagonal([0.1;0.1;0.1].^2))))
-    addFactor!(fg, [psym;nsym], pp, autoinit=false )
-end
-
-
-# Add landmarks with Bearing range measurements
-p2br2 = Pose2Point2BearingRange(Normal(0,0.03),Normal(20.0,0.5))
-addFactor!(fg, [:x18; :l1], p2br2, autoinit=false )
-
-
-
 
 tree = wipeBuildNewTree!(fg, drawpdf=true, show=true, imgs=false)
+
+
+
+## Manually organized a non-async initialization sequence
+
+docliqs = [
+5;
+3;
+4;
+6;
+2;
+5;
+1
+]
+
+
+
+# resetTreeCliquesForUpSolve!(tree)
+
+
+
+# fg_bu = deepcopy(fg)
+# tree_bu = deepcopy(tree)
+# drawTree(tree_bu)
+
+
+Niters = 100
+
+
+
+
+ts5 = @async begin
+cliq = tree.cliques[5]
+cliq.attributes["label"]
+getCliqStatus(cliq)
+clst = cliqInitSolveUp!(fg, tree, cliq, drawtree=true, limititers=Niters )
+end
+
+ts3 = @async begin
+
+cliq = tree.cliques[3]
+cliq.attributes["label"]
+getCliqStatus(cliq)
+clst = cliqInitSolveUp!(fg, tree, cliq, drawtree=true, limititers=Niters )
+
+end
+
+ts4 = @async begin
+cliq = tree.cliques[4]
+cliq.attributes["label"]
+getCliqStatus(cliq)
+clst = cliqInitSolveUp!(fg, tree, cliq, drawtree=true, limititers=Niters )
+end
+
+ts6 = @async begin
+cliq = tree.cliques[6]
+cliq.attributes["label"]
+getCliqStatus(cliq)
+clst = cliqInitSolveUp!(fg, tree, cliq, drawtree=true, limititers=Niters )
+end
+
+ts2 = @async begin
+cliq = tree.cliques[2]
+cliq.attributes["label"]
+getCliqStatus(cliq)
+clst = cliqInitSolveUp!(fg, tree, cliq, drawtree=true, limititers=Niters )
+end
+
+
+ts1 = @async begin
+cliq = tree.cliques[1]
+cliq.attributes["label"]
+getCliqStatus(cliq)
+clst = cliqInitSolveUp!(fg, tree, cliq, drawtree=true, limititers=Niters )
+end
+
+
+
+
+ts1
+ts2
+ts3
+ts4
+ts5
+ts6
+
+
+
+drawTree(tree)
+
+
+##
+
+
+
+
+
 
 
 
@@ -116,24 +166,3 @@ downMsgPassingRecursive(ett,N=100, dbg=false, drawpdf=true);
 # pl = drawPosesLandms(fg, meanmax=:max)
 # Gadfly.draw(Gadfly.PDF("/tmp/test2.pdf"),pl)  # or PNG(...)
 # @async run(`evince /tmp/test2.pdf`)
-
-
-
-## Manually organized a non-async initialization sequence
-
-docliqs = [
-14;
-13;
-12;
-11;
-4;
-9;
-3;
-10;
-8;
-7;
-6; # with async
-5; # with async
-2;
-1
-]
