bddf3802792357a8d3c8fc1995fc245dcf0a5891
dehann <dehann.fourie@gmail.com>
2019-06-03 14:08:40 -0400

    wip, dfg integration

--- examples/Beehive2D_LandmarksManual.jl
+++ examples/Beehive2D_LandmarksManual.jl
@@ -49,8 +49,8 @@ end
 
 ## start with an empty factor graph object
 fg = initfg()
-fg.isfixedlag = true
-fg.qfl = 20
+# fg.isfixedlag = true
+# fg.qfl = 20
 posecount = 0
 
 # Add the first pose :x0
@@ -63,7 +63,7 @@ addFactor!(fg, [:x0], PriorPose2( MvNormal([0.0; 0.0; 0.0],
                                            Matrix(Diagonal([0.1;0.1;0.05].^2))) ), autoinit=false )
 
 # Add landmarks with Bearing range measurements
-addVariable!(fg, :l1, Point2, labels=["LANDMARK"])
+addVariable!(fg, :l1, Point2, labels=[:LANDMARK;])
 p2br = Pose2Point2BearingRange(Normal(0,0.03),Normal(20.0,0.5))
 addFactor!(fg, [:x0; :l1], p2br, autoinit=false )
 
@@ -82,13 +82,27 @@ addFactor!(fg, [Symbol("x$(posecount-1)"); :l1], p2br2, autoinit=false )
 
 
 tree, smtasks = batchSolve!(fg, treeinit=true, drawpdf=true, show=true,
-                            returntasks=true, limititers=50,
-                            upsolve=true, downsolve=true )
+                            returntasks=true, limititers=20,
+                            upsolve=true, downsolve=false )
 0
+
+tree, smtasks = batchSolve!(fg, treeinit=true, drawpdf=true, show=true,
+                            returntasks=true, limititers=20,
+                            upsolve=false, downsolve=true )
+0
+
+# @info "Do recursive down inference over tree"
+# downMsgPassingRecursive(ExploreTreeType(fg, tree, tree.cliques[1], nothing, NBPMessage[]),
+#                           N=100, dbg=false, drawpdf=true);
+
+
 
 #
 
-drawPosesLandms(fg, meanmax=:max) |> SVG("/tmp/test.svg");  @async run(`eog /tmp/test.svg`)
+# drawPosesLandms(fg, meanmax=:max) |> SVG("/tmp/test.svg");  @async run(`eog /tmp/test.svg`)
+
+
+
 
 
 ## debug
@@ -97,13 +111,201 @@ drawPosesLandms(fg, meanmax=:max) |> SVG("/tmp/test.svg");  @async run(`eog /tmp
 
 # writeGraphPdf(fg, show=true)
 tree = wipeBuildNewTree!(fg, drawpdf=true, show=true)
+# drawTree(tree, show=true)
 # allsyms = getTreeAllFrontalSyms(fg, tree);  #allsyms = Symbol[]
 #                           recordcliqs=allsyms,
 
 
-solveCliqWithStateMachine!(fg,tree,:x0)
-plotPose(fg, :x0);
-solveCliqWithStateMachine!(fg,tree,:x2)
+
+# plotPose(fg, :x0);
+
+# tx6 = @async solveCliqWithStateMachine!(fg,tree,:x6, recordhistory=true, iters=50)
+# tx4 = @async solveCliqWithStateMachine!(fg,tree,:x4, recordhistory=true, iters=50)
+
+tx2 = @async solveCliqWithStateMachine!(fg,tree,:x2, recordhistory=true, iters=50)
+sm0, csmc0 = solveCliqWithStateMachine!(fg,tree,:x0, recordhistory=true, iters=50)
+
+
+# hist = getCliqSolveHistory(tree,:x2)
+stuff = fetch(tx2)
+hist = stuff[1].history
+printCliqHistorySummary(hist)
+
+
+
+## Debug dev
+
+import IncrementalInference: attemptCliqInitDown_StateMachine, infocsm, attemptCliqInitUp_StateMachine, prepCliqInitMsgsDown!
+
+
+@assert hist[14][3] == attemptCliqInitDown_StateMachine
+pre14 = hist[14][4]
+writeGraphPdf(pre14.cliqSubFg, engine="neato")
+
+new15 = sandboxStateMachineStep(hist,14)
+# Juno.@enter sandboxStateMachineStep(hist,14)
+
+
+prnt = getParent(tree, pre14.cliq)
+# getData(prnt[1])
+
+## HERE IS THE PROBLEM
+dwinmsgs = prepCliqInitMsgsDown!(pre14.cliqSubFg, pre14.tree, prnt[1])
+
+writeGraphPdf(pre14.cliqSubFg, show=true)
+
+
+
+
+hasVariable(pre14.cliqSubFg, :x1)
+
+
+
+
+
+
+# # make sure csmc0 agrees
+# ## why is this guy empty??
+# dwinmsgs = prepCliqInitMsgsDown!(csmc0.cliqSubFg, csmc0.tree, csmc0.tree.cliques[prnt[1].index])
+# dwinmsgs = prepCliqInitMsgsDown!(csmc0.cliqSubFg, csmc0.tree, csmc0.tree.cliques[prnt[1].index])
+
+
+
+# how did :x0 solve go
+
+printCliqHistorySummary(sm0.history)
+
+csmc0new7 = sandboxStateMachineStep(sm0.history,7)
+
+getData(csmc0new7[4].tree.cliques[2])
+
+
+
+
+
+
+
+
+0
+
+
+
+function prepCliqInitMsgsDown!(fgl::G,
+                               tree::BayesTree,
+                               cliq::Graphs.ExVertex ) where G <: AbstractDFG
+  #
+  @info "$(current_task()) Clique $(cliq.index), prepCliqInitMsgsDown!"
+  # get the current messages stored in the parent
+  currmsgs = getCliqInitUpMsgs(cliq)
+  @info "$(current_task()) Clique $(cliq.index), msg keys=$(collect(keys(currmsgs)))"
+
+  # check if any msgs should be multiplied together for the same variable
+  msgspervar = Dict{Symbol, Vector{BallTreeDensity}}()
+  for (cliqid, msgs) in currmsgs
+    @show cliqid, length(msgs)
+    for (msgsym, msg) in msgs
+      if !haskey(msgspervar, msgsym)
+        msgspervar[msgsym] = Vector{BallTreeDensity}()
+      end
+      push!(msgspervar[msgsym], msg)
+    end
+  end
+
+  @info "$(current_task()) Clique $(cliq.index), keys with msgs=$(collect(keys(msgspervar)))"
+
+  # reference to default allocated dict location
+  products = getData(cliq).downInitMsg
+  # multiply multiple messages together
+  for (msgsym, msgs) in msgspervar
+    # check if this particular down message requires msgsym
+    @show msgsym, DFG.hasVariable(fgl, msgsym)
+    if DFG.hasVariable(fgl, msgsym) #haskey(fgl.IDs, msgsym)
+      if length(msgspervar[msgsym]) > 1
+        products[msgsym] = manifoldProduct(msgs, getManifolds(fgl, msgsym))
+      else
+        products[msgsym] = msgs[1]
+      end
+    else
+      # not required, therefore remove from message to avoid confusion
+      if haskey(products, msgsym)
+        delete!(products, msgsym)
+      end
+    end
+  end
+
+  @info "$(current_task()) Clique $(cliq.index), product keys=$(collect(keys(products)))"
+  return products
+end
+
+0
+
+
+function attemptCliqInitDown_StateMachine(csmc::CliqStateMachineContainer)
+  #
+  # should never happen to
+  setCliqDrawColor(csmc.cliq, "green")
+  csmc.drawtree ? drawTree(csmc.tree, show=false) : nothing
+
+  # initialize clique in downward direction
+  # not if parent also needs downward init message
+  infocsm(csmc, "8a, needs down message -- attempt down init")
+  prnt = getParent(csmc.tree, csmc.cliq)[1]
+  @show dwinmsgs = prepCliqInitMsgsDown!(csmc.cliqSubFg, csmc.tree, prnt)
+
+  @show cliqst = doCliqInitDown!(csmc.cliqSubFg, csmc.cliq, dwinmsgs)
+  # TODO: transfer values changed in the cliques should be transfered to the tree in proc 1 here.
+
+  # TODO: maybe this should be here?
+  setCliqStatus!(csmc.cliq, cliqst)
+
+  # TODO move out
+  children = getChildren(csmc.tree, csmc.cliq)
+  if areCliqChildrenNeedDownMsg(children)
+    # set messages if children :needdownmsg
+    infocsm(csmc, "8a, doCliqInitDown! -- must set messages for future down init")
+    # construct init's up msg to place in parent from initialized separator variables
+    msg = prepCliqInitMsgsUp(csmc.cliqSubFg, csmc.cliq) # , tree,
+
+    infocsm(csmc, "8a, putting fake upinitmsg in this cliq, msgs labels $(collect(keys(msg)))")
+    # set fake up and notify down status
+    setCliqUpInitMsgs!(csmc.cliq, csmc.cliq.index, msg)
+    # setCliqStatus!(csmc.cliq, cliqst)
+    setCliqDrawColor(csmc.cliq, "sienna")
+    csmc.drawtree ? drawTree(csmc.tree, show=false) : nothing
+
+    notifyCliqDownInitStatus!(csmc.cliq, cliqst)
+
+    infocsm(csmc, "8a, after down init attempt, $cliqst.")
+  end
+
+  return attemptCliqInitUp_StateMachine
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+using Graphs
+
+drawStateMachineHistory(hist)
+
 
 drawTree(tree, imgs=true)
 
