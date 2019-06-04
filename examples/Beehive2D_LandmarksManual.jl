d439f1b50c9411d8fb56cf48f513af9cf2a276b9
dehann <dehann.fourie@gmail.com>
2019-06-04 18:21:02 -0400

    wip for example with dfg integration in late stages

--- examples/Beehive2D_LandmarksManual.jl
+++ examples/Beehive2D_LandmarksManual.jl
@@ -1,7 +1,7 @@
 
 # using Revise
 using Distributed
-addprocs(3)
+addprocs(2)
 
 ##
 
@@ -31,18 +31,18 @@ function driveHex(fgl, posecount::Int; steps::Int=5)
 end
 
 
-function offsetHexLeg(fgl::FactorGraph, posecount::Int; direction=:right)
+function offsetHexLeg(dfg::G, posecount::Int; direction=:right) where G <: AbstractDFG
     psym = Symbol("x$(posecount-1)")
     nsym = Symbol("x$(posecount)")
     posecount += 1
-    addVariable!(fgl, nsym, Pose2)
+    addVariable!(dfg, nsym, Pose2)
     pp = nothing
     if direction == :right
         pp = Pose2Pose2(MvNormal([10.0;0;-pi/3], Matrix(Diagonal([0.1;0.1;0.1].^2))))
     elseif direction == :left
         pp = Pose2Pose2(MvNormal([10.0;0;pi/3], Matrix(Diagonal([0.1;0.1;0.1].^2))))
     end
-    addFactor!(fgl, [psym; nsym], pp, autoinit=false )
+    addFactor!(dfg, [psym; nsym], pp, autoinit=false )
     return posecount
 end
 
@@ -78,18 +78,18 @@ p2br2 = Pose2Point2BearingRange(Normal(0,0.03),Normal(20.0,0.5))
 addFactor!(fg, [Symbol("x$(posecount-1)"); :l1], p2br2, autoinit=false )
 
 
-
+writeGraphPdf(fg,engine="neato")
 
 
 tree, smtasks = batchSolve!(fg, treeinit=true, drawpdf=true, show=true,
-                            returntasks=true, limititers=20,
-                            upsolve=true, downsolve=false )
+                            returntasks=true, limititers=50,
+                            upsolve=true, downsolve=true )
 0
 
-tree, smtasks = batchSolve!(fg, treeinit=true, drawpdf=true, show=true,
-                            returntasks=true, limititers=20,
-                            upsolve=false, downsolve=true )
-0
+# tree, smtasks = batchSolve!(fg, treeinit=true, drawpdf=true, show=true,
+#                             returntasks=true, limititers=20,
+#                             upsolve=false, downsolve=true )
+# 0
 
 # @info "Do recursive down inference over tree"
 # downMsgPassingRecursive(ExploreTreeType(fg, tree, tree.cliques[1], nothing, NBPMessage[]),
@@ -103,14 +103,11 @@ tree, smtasks = batchSolve!(fg, treeinit=true, drawpdf=true, show=true,
 
 
 
-
-
 ## debug
 
 
-
 # writeGraphPdf(fg, show=true)
-tree = wipeBuildNewTree!(fg, drawpdf=true, show=true)
+# tree = wipeBuildNewTree!(fg, drawpdf=true, show=true)
 # drawTree(tree, show=true)
 # allsyms = getTreeAllFrontalSyms(fg, tree);  #allsyms = Symbol[]
 #                           recordcliqs=allsyms,
@@ -121,212 +118,27 @@ tree = wipeBuildNewTree!(fg, drawpdf=true, show=true)
 
 # tx6 = @async solveCliqWithStateMachine!(fg,tree,:x6, recordhistory=true, iters=50)
 # tx4 = @async solveCliqWithStateMachine!(fg,tree,:x4, recordhistory=true, iters=50)
-
-tx2 = @async solveCliqWithStateMachine!(fg,tree,:x2, recordhistory=true, iters=50)
-sm0, csmc0 = solveCliqWithStateMachine!(fg,tree,:x0, recordhistory=true, iters=50)
-
-
-# hist = getCliqSolveHistory(tree,:x2)
-stuff = fetch(tx2)
-hist = stuff[1].history
-printCliqHistorySummary(hist)
-
-
-
-## Debug dev
-
-import IncrementalInference: attemptCliqInitDown_StateMachine, infocsm, attemptCliqInitUp_StateMachine, prepCliqInitMsgsDown!
-
-
-@assert hist[14][3] == attemptCliqInitDown_StateMachine
-pre14 = hist[14][4]
-writeGraphPdf(pre14.cliqSubFg, engine="neato")
-
-new15 = sandboxStateMachineStep(hist,14)
-# Juno.@enter sandboxStateMachineStep(hist,14)
-
-
-prnt = getParent(tree, pre14.cliq)
-# getData(prnt[1])
-
-## HERE IS THE PROBLEM
-dwinmsgs = prepCliqInitMsgsDown!(pre14.cliqSubFg, pre14.tree, prnt[1])
-
-writeGraphPdf(pre14.cliqSubFg, show=true)
-
-
-
-
-hasVariable(pre14.cliqSubFg, :x1)
-
-
-
-
-
-
-# # make sure csmc0 agrees
-# ## why is this guy empty??
-# dwinmsgs = prepCliqInitMsgsDown!(csmc0.cliqSubFg, csmc0.tree, csmc0.tree.cliques[prnt[1].index])
-# dwinmsgs = prepCliqInitMsgsDown!(csmc0.cliqSubFg, csmc0.tree, csmc0.tree.cliques[prnt[1].index])
-
-
-
-# how did :x0 solve go
-
-printCliqHistorySummary(sm0.history)
-
-csmc0new7 = sandboxStateMachineStep(sm0.history,7)
-
-getData(csmc0new7[4].tree.cliques[2])
-
-
-
-
-
-
-
-
-0
-
-
-
-function prepCliqInitMsgsDown!(fgl::G,
-                               tree::BayesTree,
-                               cliq::Graphs.ExVertex ) where G <: AbstractDFG
-  #
-  @info "$(current_task()) Clique $(cliq.index), prepCliqInitMsgsDown!"
-  # get the current messages stored in the parent
-  currmsgs = getCliqInitUpMsgs(cliq)
-  @info "$(current_task()) Clique $(cliq.index), msg keys=$(collect(keys(currmsgs)))"
-
-  # check if any msgs should be multiplied together for the same variable
-  msgspervar = Dict{Symbol, Vector{BallTreeDensity}}()
-  for (cliqid, msgs) in currmsgs
-    @show cliqid, length(msgs)
-    for (msgsym, msg) in msgs
-      if !haskey(msgspervar, msgsym)
-        msgspervar[msgsym] = Vector{BallTreeDensity}()
-      end
-      push!(msgspervar[msgsym], msg)
-    end
-  end
-
-  @info "$(current_task()) Clique $(cliq.index), keys with msgs=$(collect(keys(msgspervar)))"
-
-  # reference to default allocated dict location
-  products = getData(cliq).downInitMsg
-  # multiply multiple messages together
-  for (msgsym, msgs) in msgspervar
-    # check if this particular down message requires msgsym
-    @show msgsym, DFG.hasVariable(fgl, msgsym)
-    if DFG.hasVariable(fgl, msgsym) #haskey(fgl.IDs, msgsym)
-      if length(msgspervar[msgsym]) > 1
-        products[msgsym] = manifoldProduct(msgs, getManifolds(fgl, msgsym))
-      else
-        products[msgsym] = msgs[1]
-      end
-    else
-      # not required, therefore remove from message to avoid confusion
-      if haskey(products, msgsym)
-        delete!(products, msgsym)
-      end
-    end
-  end
-
-  @info "$(current_task()) Clique $(cliq.index), product keys=$(collect(keys(products)))"
-  return products
-end
-
-0
-
-
-function attemptCliqInitDown_StateMachine(csmc::CliqStateMachineContainer)
-  #
-  # should never happen to
-  setCliqDrawColor(csmc.cliq, "green")
-  csmc.drawtree ? drawTree(csmc.tree, show=false) : nothing
-
-  # initialize clique in downward direction
-  # not if parent also needs downward init message
-  infocsm(csmc, "8a, needs down message -- attempt down init")
-  prnt = getParent(csmc.tree, csmc.cliq)[1]
-  @show dwinmsgs = prepCliqInitMsgsDown!(csmc.cliqSubFg, csmc.tree, prnt)
-
-  @show cliqst = doCliqInitDown!(csmc.cliqSubFg, csmc.cliq, dwinmsgs)
-  # TODO: transfer values changed in the cliques should be transfered to the tree in proc 1 here.
-
-  # TODO: maybe this should be here?
-  setCliqStatus!(csmc.cliq, cliqst)
-
-  # TODO move out
-  children = getChildren(csmc.tree, csmc.cliq)
-  if areCliqChildrenNeedDownMsg(children)
-    # set messages if children :needdownmsg
-    infocsm(csmc, "8a, doCliqInitDown! -- must set messages for future down init")
-    # construct init's up msg to place in parent from initialized separator variables
-    msg = prepCliqInitMsgsUp(csmc.cliqSubFg, csmc.cliq) # , tree,
-
-    infocsm(csmc, "8a, putting fake upinitmsg in this cliq, msgs labels $(collect(keys(msg)))")
-    # set fake up and notify down status
-    setCliqUpInitMsgs!(csmc.cliq, csmc.cliq.index, msg)
-    # setCliqStatus!(csmc.cliq, cliqst)
-    setCliqDrawColor(csmc.cliq, "sienna")
-    csmc.drawtree ? drawTree(csmc.tree, show=false) : nothing
-
-    notifyCliqDownInitStatus!(csmc.cliq, cliqst)
-
-    infocsm(csmc, "8a, after down init attempt, $cliqst.")
-  end
-
-  return attemptCliqInitUp_StateMachine
-end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-using Graphs
-
-drawStateMachineHistory(hist)
-
-
-drawTree(tree, imgs=true)
-
-cliq = whichCliq(tree, :x1)
-
-cliqd = getData(cliq)
-
-
+#
+# tx2 = @async solveCliqWithStateMachine!(fg,tree,:x2, recordhistory=true, iters=50)
+# sm0, csmc0 = solveCliqWithStateMachine!(fg,tree,:x0, recordhistory=true, iters=50)
+#
+# # hist = getCliqSolveHistory(tree,:x2)
+# stuff = fetch(tx2)
+# hist = stuff[1].history
+# printCliqHistorySummary(hist)
+# printCliqHistorySummary(sm0.history)
+# csmc0new7 = sandboxStateMachineStep(sm0.history,7)
+# getData(csmc0new7[4].tree.cliques[2])
+#
+# using Graphs
+# drawStateMachineHistory(hist)
+# animateCliqStateMachines(tree, [:x2;:x1;:x3], frames=100)
 
 # fihs = filterHistAllToArray(tree, [:x0;:x1;:x2;:x3;:x4;:x6], slowCliqIfChildrenNotUpsolved_StateMachine)
 # printCliqHistorySummary(fihs)
 
 
-##
 
-# drawTree(tree)
-
-# printCliqHistorySummary(tree, :x2)
-# cliq = whichCliq(tree, :x3)
-# hist = getData(cliq).statehistory
-# animateCliqStateMachines(tree, [:x2;:x1;:x3], frames=100)
 
 
 
@@ -335,7 +147,7 @@ cliqd = getData(cliq)
 posecount = offsetHexLeg(fg, posecount, direction=:right)
 
 # Add landmarks with Bearing range measurements
-addVariable!(fg, :l2, Point2, labels=["LANDMARK"])
+addVariable!(fg, :l2, Point2, labels=[:LANDMARK])
 p2br = Pose2Point2BearingRange(Normal(0,0.03),Normal(20.0,0.5))
 addFactor!(fg, [Symbol("x$(posecount-1)"); :l2], p2br, autoinit=false )
 
@@ -357,52 +169,20 @@ posecount = driveHex(fg, posecount, steps=5)
 # allsyms = getTreeAllFrontalSyms(fg, tree);  #allsyms = Symbol[]
 #                 recordcliqs=allsyms,
 
+# tree = wipeBuildNewTree!(fg, drawpdf=true)
+# whichCliq(tree, :x11)
+
 
 tree, smtasks = batchSolve!(fg, treeinit=true, drawpdf=true, show=true,
-                  returntasks=true, limititers=50, recordcliqs=[:x5;:x11;:x12;:x13],
-                  upsolve=true, downsolve=true  )
+                  returntasks=true, limititers=50, recordcliqs=[:x5;:x11;:x12;:x13;:x10],
+                  upsolve=true, downsolve=true) # , skipcliqids=[1;3;8;9]  )
 0
+
+
 
 drawPosesLandms(fg, meanmax=:max) |> SVG("/tmp/test.svg");
 
 # drawTree(tree)
-
-
-
-# fihs = filterHistAllToArray(tree, allsyms, slowCliqIfChildrenNotUpsolved_StateMachine)
-# printCliqHistorySummary(fihs)
-
-
-
-
-
-printCliqHistorySummary(tree,:x5)
-printCliqHistorySummary(tree,:x11)
-printCliqHistorySummary(tree,:x12)
-printCliqHistorySummary(tree,:x13)
-#
-# # hist = getCliqSolveHistory(tree, :x12)
-# # sandboxCliqResolveStep(tree, :x12, 1)
-# animateCliqStateMachines(tree,[:x12;:x11;:x8],frames=100)
-#
-#
-# 0
-##
-
-
-
-
-
-
-
-#
-#
-# tree = wipeBuildNewTree!(fg, drawpdf=true, imgs=false)
-# # drawTree(tree, imgs=false)
-#
-
-
-
 
 
 
@@ -418,7 +198,7 @@ addFactor!(fg, [Symbol("x$(posecount-1)"); :l2], p2br2, autoinit=false )
 posecount = offsetHexLeg(fg, posecount, direction=:right)
 
 # Add landmarks with Bearing range measurements
-addVariable!(fg, :l3, Point2, labels=["LANDMARK"])
+addVariable!(fg, :l3, Point2, labels=[:LANDMARK])
 p2br = Pose2Point2BearingRange(Normal(0,0.03),Normal(20.0,0.5))
 addFactor!(fg, [Symbol("x$(posecount-1)"); :l3], p2br, autoinit=false )
 
@@ -455,7 +235,7 @@ addFactor!(fg, [:x18; :l1], p2br2, autoinit=false )
 
 # new sighting
 
-addVariable!(fg, :l0, Point2, labels=["LANDMARK"])
+addVariable!(fg, :l0, Point2, labels=[:LANDMARK])
 p2br = Pose2Point2BearingRange(Normal(0,0.03),Normal(20.0,0.5))
 addFactor!(fg, [:x5; :l0], p2br, autoinit=false )
 
@@ -487,7 +267,7 @@ posecount = offsetHexLeg(fg, posecount, direction=:right)
 
 
 # Add landmarks with Bearing range measurements
-addVariable!(fg, :l4, Point2, labels=["LANDMARK"])
+addVariable!(fg, :l4, Point2, labels=[:LANDMARK])
 p2br = Pose2Point2BearingRange(Normal(0,0.03),Normal(20.0,0.5))
 addFactor!(fg, [Symbol("x$(posecount-1)"); :l4], p2br, autoinit=false )
 
@@ -539,7 +319,7 @@ drawPosesLandms(fg, meanmax=:max) |> SVG("/tmp/test.svg") #|| @async run(`eog /t
 posecount = offsetHexLeg(fg, posecount, direction=:right)
 
 # Add landmarks with Bearing range measurements
-addVariable!(fg, :l5, Point2, labels=["LANDMARK"])
+addVariable!(fg, :l5, Point2, labels=[:LANDMARK])
 p2br = Pose2Point2BearingRange(Normal(0,0.03),Normal(20.0,0.5))
 addFactor!(fg, [Symbol("x$(posecount-1)"); :l5], p2br, autoinit=false )
 
@@ -605,7 +385,7 @@ drawPosesLandms(fg, meanmax=:max) |> SVG("/tmp/test.svg"); # @async run(`eog /tm
 posecount = offsetHexLeg(fg, posecount, direction=:right)
 
 # Add landmarks with Bearing range measurements
-addVariable!(fg, :l6, Point2, labels=["LANDMARK"])
+addVariable!(fg, :l6, Point2, labels=[:LANDMARK])
 p2br = Pose2Point2BearingRange(Normal(0,0.03),Normal(20.0,0.5))
 addFactor!(fg, [Symbol("x$(posecount-1)"); :l6], p2br, autoinit=false )
 
@@ -720,7 +500,7 @@ drawPosesLandms(fg, meanmax=:max) |> SVG("/tmp/test.svg"); # @async run(`eog /tm
 
 
 ## add sightings
-addVariable!(fg, :l8, Point2, labels=["LANDMARK"])
+addVariable!(fg, :l8, Point2, labels=[:LANDMARK])
 p2br2 = Pose2Point2BearingRange(Normal(0,0.03),Normal(20.0,0.5))
 addFactor!(fg, [:x2; :l8], p2br2, autoinit=false )
 
