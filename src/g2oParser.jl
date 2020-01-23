dd23b8581ac599ea0a725d2d216b9b0e504aa78a
dehann <dehann.fourie@gmail.com>
2020-01-23 12:04:27 -0500

    start with export handle

--- src/g2oParser.jl
+++ src/g2oParser.jl
@@ -16,8 +16,17 @@
 ## Export g2o functions
 
 
+"""
+    $SIGNATURES
 
+Export a factor graph to g2o file format.
 
+Note:
+- This funtion only supports Gaussian (i.e. Normal/MvNormal) factors, unpredictable witchcraft is used in other cases such as `AliasingScalarSampler` factor models.
+"""
+function exportG2o(dfg::AbstractDFG)
+  error("Not implemented yet")
+end
 
 
 
