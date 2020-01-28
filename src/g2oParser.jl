637e99731e4aa0c482ab16949fc7971880240967
antonio teran <teran@mit.edu>
2020-01-28 18:49:55 -0500

    initial implementation of EDGE_SE2 parser

--- src/g2oParser.jl
+++ src/g2oParser.jl
@@ -1,7 +1,7 @@
 # This file is for g2o integration. For more information on the actual file
 # formats, refer to: https://github.com/RainerKuemmerle/g2o/wiki/File-Format
 
-export importG2o, exportG2o, findCommand
+export importG2o, exportG2o, findCommand, parseG2oInstruction!
 
 ## Common functions for g2o parsing
 
@@ -30,7 +30,11 @@ function findCommand(comm::Symbol)
 end
 
 
-## Import g2o functions
+"""
+    $SIGNATURES
+
+Read in every line from a g2o file.
+"""
 function importG2o(input_file::String)
     instructions = []
     # Read input file line by line.
@@ -45,8 +49,47 @@ function importG2o(input_file::String)
     return instructions
 end
 
-function parseG2oInstruction(instruction::Array{SubString{String},1})
-    #
+"""
+    $SIGNATURES
+
+Parses a single g2o instruction to add information to factor graph.
+"""
+function parseG2oInstruction!(fg::AbstractDFG,
+                              instruction::Array{SubString{String},1})
+    if instruction[1] == "EDGE_SE2"
+        # Need to add a relative pose measurement between two variables.
+        # Parse all of the variables starting with the symbols.
+        from_pose = Symbol("x", instruction[2])
+        to_pose = Symbol("x", instruction[3])
+
+        # Parse the mean values for the relative pose measurements.
+        x_state = parse(Float64, instruction[4])
+        y_state = parse(Float64, instruction[5])
+        theta_state = parse(Float64, instruction[6])
+
+        # Parse the information matrix and take inverse to obtain covariance.
+        info_mat = [parse(Float64, instruction[7]) parse(Float64, instruction[8])  parse(Float64, instruction[9]);
+                    parse(Float64, instruction[8]) parse(Float64, instruction[10]) parse(Float64, instruction[11]);
+                    parse(Float64, instruction[9]) parse(Float64, instruction[11]) parse(Float64, instruction[12])]
+        cov_mat = inv(info_mat)
+        # NOTE workaround to ensure cov_mat is Hermitian -- TODO use inf_mat directly for MvNormal
+        cov_mat += cov_mat'
+        cov_mat ./= 2
+
+        # Make sure both variables are in the FG. Otherwise add them.
+        if (from_pose in ls(fg)) == false
+            addVariable!(fg, from_pose, Pose2)
+        end
+        if (to_pose in ls(fg)) == false
+            addVariable!(fg, to_pose, Pose2)
+        end
+
+        # Add a factor between the two poses with the relative measurement.
+        measurement = MvNormal([x_state; y_state; theta_state], cov_mat)
+        rel_pose_factor = Pose2Pose2(measurement)
+        addFactor!(fg, [from_pose, to_pose], rel_pose_factor)
+    end
+    return fg
 end
 
 
