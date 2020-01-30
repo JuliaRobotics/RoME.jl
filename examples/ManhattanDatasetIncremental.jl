aae6b96683a1d96b5d814c14322d862e38b1c60a
antonio teran <teran@mit.edu>
2020-01-29 22:07:58 -0500

    enclose in function scope to eliminate undefined errors

--- examples/ManhattanDatasetIncremental.jl
+++ examples/ManhattanDatasetIncremental.jl
@@ -20,51 +20,28 @@ latex_fonts = Theme(major_label_font="CMU Serif", major_label_font_size=16pt,
                     key_label_font="CMU Serif", key_label_font_size=10pt)
 Gadfly.push_theme(latex_fonts)
 
-# Choose where to save the step's data.
-data_logpath = "/media/data2/tonio_results/manhattan-$(now())"
-# Create initial factor graph with specified logging path.
-fg = LightDFG{SolverParams}(params=SolverParams(logpath=data_logpath))
+function go(final_timestep::Integer)
+    # Choose where to save the step's data.
+    data_logpath = "/media/data2/tonio_results/manhattan-$(now())"
+    # Create initial factor graph with specified logging path.
+    fg = LightDFG{SolverParams}(params=SolverParams(logpath=data_logpath))
 
-# Add initial variable with a prior measurement to anchor the graph.
-addVariable!(fg, :x0, Pose2)
-initial_pose = MvNormal([0.0; 0.0; 0.0], Matrix(Diagonal([0.1;0.1;0.05].^2)))
-addFactor!(fg, [:x0], PriorPose2(initial_pose))
+    # Add initial variable with a prior measurement to anchor the graph.
+    addVariable!(fg, :x0, Pose2)
+    initial_pose = MvNormal([0.0; 0.0; 0.0], Matrix(Diagonal([0.1;0.1;0.05].^2)))
+    addFactor!(fg, [:x0], PriorPose2(initial_pose))
 
-# Add the next measurement to the graph.
-parseG2oInstruction!(fg, instructions[1])
-
-# And store a picture of the hitherto graph.
-padded_step = lpad(1, 4, "0")
-drawGraph(fg, show=false, engine="sfdp",
-          filepath="$(getLogPath(fg))/graph$(padded_step).pdf")
-
-# Solve the graph, and save a copy of the tree.
-saveDFG(fg, "$(getLogPath(fg))/fg-before-solve$(padded_step)")
-tree, smt, hist = solveTree!(fg)
-saveDFG(fg, "$(getLogPath(fg))/fg-after-solve$(padded_step)")
-saveTree(tree, "$(getLogPath(fg))/tree$(padded_step).jld2")
-drawTree(tree, show=false, filepath="$(getLogPath(fg))/bt$(padded_step).pdf")
-
-# Just store some quick plots.
-pl1 = drawPoses(fg, spscale=0.6)
-Gadfly.draw(PDF("$(getLogPath(fg))/poses$(padded_step).pdf", 20cm, 10cm), pl1)
-
-plkde = plotKDE(fg, ls(fg), dims=[1;2], levels=3)
-Gadfly.draw(PDF("$(getLogPath(fg))/kde$(padded_step).pdf", 20cm, 10cm), plkde)
-
-# Run the loop for the remaining time steps.
-for i in 2:final_timestep
     # Add the next measurement to the graph.
-    parseG2oInstruction!(fg, instructions[i])
+    parseG2oInstruction!(fg, instructions[1])
 
     # And store a picture of the hitherto graph.
-    padded_step = lpad(i, 4, "0")
+    padded_step = lpad(1, 4, "0")
     drawGraph(fg, show=false, engine="sfdp",
               filepath="$(getLogPath(fg))/graph$(padded_step).pdf")
 
     # Solve the graph, and save a copy of the tree.
     saveDFG(fg, "$(getLogPath(fg))/fg-before-solve$(padded_step)")
-    tree, smt, hist = solveTree!(fg, tree)
+    tree, smt, hist = solveTree!(fg)
     saveDFG(fg, "$(getLogPath(fg))/fg-after-solve$(padded_step)")
     saveTree(tree, "$(getLogPath(fg))/tree$(padded_step).jld2")
     drawTree(tree, show=false, filepath="$(getLogPath(fg))/bt$(padded_step).pdf")
@@ -75,4 +52,32 @@ for i in 2:final_timestep
 
     plkde = plotKDE(fg, ls(fg), dims=[1;2], levels=3)
     Gadfly.draw(PDF("$(getLogPath(fg))/kde$(padded_step).pdf", 20cm, 10cm), plkde)
+
+    # Run the loop for the remaining time steps.
+    for i in 2:final_timestep
+        # Add the next measurement to the graph.
+        parseG2oInstruction!(fg, instructions[i])
+
+        # And store a picture of the hitherto graph.
+        padded_step = lpad(i, 4, "0")
+        drawGraph(fg, show=false, engine="sfdp",
+                  filepath="$(getLogPath(fg))/graph$(padded_step).pdf")
+
+        # Solve the graph, and save a copy of the tree.
+        saveDFG(fg, "$(getLogPath(fg))/fg-before-solve$(padded_step)")
+        tree, smt, hist = solveTree!(fg, tree)
+        saveDFG(fg, "$(getLogPath(fg))/fg-after-solve$(padded_step)")
+        saveTree(tree, "$(getLogPath(fg))/tree$(padded_step).jld2")
+        drawTree(tree, show=false, filepath="$(getLogPath(fg))/bt$(padded_step).pdf")
+
+        # Just store some quick plots.
+        pl1 = drawPoses(fg, spscale=0.6)
+        Gadfly.draw(PDF("$(getLogPath(fg))/poses$(padded_step).pdf", 20cm, 10cm), pl1)
+
+        plkde = plotKDE(fg, ls(fg), dims=[1;2], levels=3)
+        Gadfly.draw(PDF("$(getLogPath(fg))/kde$(padded_step).pdf", 20cm, 10cm), plkde)
+    end
 end
+
+# Run within a function to avoid undefined variable errors.
+go(final_timestep)
