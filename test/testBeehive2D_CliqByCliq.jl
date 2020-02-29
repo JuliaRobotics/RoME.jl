1e74f05ce72a4cb68f8efe9a636dc88b223e837f
dehann <dehann.fourie@gmail.com>
2020-02-29 16:18:15 -0500

    update test (tighter)

--- test/testBeehive2D_CliqByCliq.jl
+++ test/testBeehive2D_CliqByCliq.jl
@@ -26,59 +26,56 @@ getSolverParams(fg).async = false
 # direct solve would be
 # getSolverParams(fg).async = true ## MANUAL TEMP ONLY
 tree, smt, hist = solveTree!(fg) #, recordcliqs=ls(fg))
+tree, smt, hist = solveTree!(fg) #, recordcliqs=ls(fg))
 
 # tree.cliques[3]
 # drawTree(tree, show=true)
 # smt[3]
 
-@test 40 < sum(-3.0 .< getPoints(getKDE(fg, :x0))[1,:] .< 3.0)
-@test 40 < sum(-3.0 .< getPoints(getKDE(fg, :x0))[2,:] .< 3.0)
-@test 40 < sum(-0.3 .< getPoints(getKDE(fg, :x0))[3,:] .< 0.3)
+@test 55 < sum(-3.0 .< getPoints(getKDE(fg, :x0))[1,:] .< 3.0)
+@test 55 < sum(-3.0 .< getPoints(getKDE(fg, :x0))[2,:] .< 3.0)
+@test 55 < sum(-0.3 .< getPoints(getKDE(fg, :x0))[3,:] .< 0.3)
 
-@test 40 < sum(7.0 .< getPoints(getKDE(fg, :x1))[1,:] .< 13.0)
-@test 40 < sum(-3.0 .< getPoints(getKDE(fg, :x1))[2,:] .< 3.0)
-@test 40 < sum(0.7 .< getPoints(getKDE(fg, :x1))[3,:] .< 1.3)
+@test 55 < sum(7.0 .< getPoints(getKDE(fg, :x1))[1,:] .< 13.0)
+@test 55 < sum(-3.0 .< getPoints(getKDE(fg, :x1))[2,:] .< 3.0)
+@test 55 < sum(0.7 .< getPoints(getKDE(fg, :x1))[3,:] .< 1.3)
 
-@test 40 < sum(12.0 .< getPoints(getKDE(fg, :x2))[1,:] .< 18.0)
-@test 40 < sum(6.0 .< getPoints(getKDE(fg, :x2))[2,:] .< 11.0)
-@test 40 < sum(1.8 .< getPoints(getKDE(fg, :x2))[3,:] .< 2.4)
+@test 55 < sum(12.0 .< getPoints(getKDE(fg, :x2))[1,:] .< 18.0)
+@test 55 < sum(6.0 .< getPoints(getKDE(fg, :x2))[2,:] .< 11.0)
+@test 55 < sum(1.8 .< getPoints(getKDE(fg, :x2))[3,:] .< 2.4)
 
-@test 40 < sum(7.0 .< getPoints(getKDE(fg, :x3))[1,:] .< 13.0)
-@test 40 < sum(15.0 .< getPoints(getKDE(fg, :x3))[2,:] .< 20.0)
-# @test 40 < sum(-0.3 .< getPoints(getKDE(fg, :x3))[3,:] .< 0.3)
+@test 55 < sum(7.0 .< getPoints(getKDE(fg, :x3))[1,:] .< 13.0)
+@test 55 < sum(15.0 .< getPoints(getKDE(fg, :x3))[2,:] .< 20.0)
+# @test 55 < sum(-0.3 .< getPoints(getKDE(fg, :x3))[3,:] .< 0.3)
 
-@test 40 < sum(-5.0 .< getPoints(getKDE(fg, :x4))[1,:] .< 5.0)
-@test 40 < sum(13.0 .< getPoints(getKDE(fg, :x4))[2,:] .< 22.0)
-@test 40 < sum(-2.8 .< getPoints(getKDE(fg, :x4))[3,:] .< -1.5)
+@test 55 < sum(-5.0 .< getPoints(getKDE(fg, :x4))[1,:] .< 5.0)
+@test 55 < sum(13.0 .< getPoints(getKDE(fg, :x4))[2,:] .< 22.0)
+@test 55 < sum(-2.8 .< getPoints(getKDE(fg, :x4))[3,:] .< -1.5)
 
-@test 40 < sum(-8.0 .< getPoints(getKDE(fg, :x5))[1,:] .< -2.0)
-@test 40 < sum(6.0 .< getPoints(getKDE(fg, :x5))[2,:] .< 11.0)
-@test 40 < sum(-1.3 .< getPoints(getKDE(fg, :x5))[3,:] .< -0.7)
+@test 55 < sum(-8.0 .< getPoints(getKDE(fg, :x5))[1,:] .< -2.0)
+@test 55 < sum(6.0 .< getPoints(getKDE(fg, :x5))[2,:] .< 11.0)
+@test 55 < sum(-1.3 .< getPoints(getKDE(fg, :x5))[3,:] .< -0.7)
 
-@test 40 < sum(-3.0 .< getPoints(getKDE(fg, :x6))[1,:] .< 3.0)
-@test 40 < sum(-3.0 .< getPoints(getKDE(fg, :x6))[2,:] .< 3.0)
-@test 40 < sum(-0.3 .< getPoints(getKDE(fg, :x6))[3,:] .< 0.3)
+@test 55 < sum(-3.0 .< getPoints(getKDE(fg, :x6))[1,:] .< 3.0)
+@test 55 < sum(-3.0 .< getPoints(getKDE(fg, :x6))[2,:] .< 3.0)
+@test 55 < sum(-0.3 .< getPoints(getKDE(fg, :x6))[3,:] .< 0.3)
 
-@test 40 < sum(17.0 .< getPoints(getKDE(fg, :l1))[1,:] .< 23.0)
-@test 40 < sum(-5.0 .< getPoints(getKDE(fg, :l1))[2,:] .< 5.0)
-
-
-
-# #  Do some plotting
-# using RoMEPlotting
-# #
-# Gadfly.set_default_plot_size(35cm,20cm)
-# pl = drawPosesLandms(fg, drawhist=true)
-
-# pl |> PDF("/tmp/caesar/test.pdf");  @async run(`evince /tmp/caesar/test.pdf`)
-# pl |> PNG("/tmp/caesar/test.png");
-# drawTree(tree, imgs=true)
+@test 55 < sum(17.0 .< getPoints(getKDE(fg, :l1))[1,:] .< 23.0)
+@test 55 < sum(-5.0 .< getPoints(getKDE(fg, :l1))[2,:] .< 5.0)
 
 
 
 
 0
 end # testset
+
+
+#
+# #  Do some plotting
+# using RoMEPlotting
+# #
+# Gadfly.set_default_plot_size(35cm,20cm)
+# pl = drawPosesLandms(fg, drawhist=true)
 
 
 
