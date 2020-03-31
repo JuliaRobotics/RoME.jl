e3a284e7642184923fbddc40a8cd62d0c23153aa
dehann <dehann.fourie@gmail.com>
2020-03-31 18:49:20 -0400

    update commands

--- examples/commands.sh
+++ examples/commands.sh
@@ -4,41 +4,48 @@ mmLasso4() {
   JULIA_NUM_THREADS=2 julia -O3 -p4 OutlierVsMultimodal.jl $*
 }
 
-mmLassoMC4() {
+mmLassoMC5() {
   sleep  1; mmLasso4 $* &
-  sleep 30; mmLasso4 $*
+  sleep 30; mmLasso4 $* &
   sleep 60; mmLasso4 $* &
-  sleep 90; mmLasso4 $*
+  sleep 90; mmLasso4 $* &
+  sleep 120; mmLasso4 $*
 }
 
-mmLassoMC4_alt5() {
-  mmLassoMC4 --altmode 0.5 $*
+mmLassoMC5_alt5() {
+  mmLassoMC5 --altmode 0.5 $*
 }
 
-mmLassoMC4_alt6() {
-  mmLassoMC4 --altmode 0.6 $*
+mmLassoMC5_alt6() {
+  mmLassoMC5 --altmode 0.6 $*
 }
 
-mmLassoMC4_alt7() {
-  mmLassoMC4 --altmode 0.7 $*
+mmLassoMC5_alt7() {
+  mmLassoMC5 --altmode 0.7 $*
 }
 
-mmLassoMC4_alt8() {
-  mmLassoMC4 --altmode 0.8 $*
+mmLassoMC5_alt8() {
+  mmLassoMC5 --altmode 0.8 $*
 }
 
-mmLassoMC4_alt9() {
-  mmLassoMC4 --altmode 0.9 $*
+mmLassoMC5_alt9() {
+  mmLassoMC5 --altmode 0.9 $*
 }
 
-mmLassoMC4AnalysisNH() {
-  mmLassoMC4_alt5 --spreadNH 1.0 &
-  mmLassoMC4_alt7 --spreadNH 1.0 &
-  mmLassoMC4_alt9 --spreadNH 1.0
-  mmLassoMC4_alt5 --spreadNH 10.0 &
-  mmLassoMC4_alt7 --spreadNH 10.0 &
-  mmLassoMC4_alt9 --spreadNH 10.0
-  mmLassoMC4_alt5 --spreadNH 25.0 &
-  mmLassoMC4_alt7 --spreadNH 25.0 &
-  mmLassoMC4_alt9 --spreadNH 25.0
+mmLassoMC5AnalysisNH() {
+  mmLassoMC5_alt5 --spreadNH 5.0 $* &
+  mmLassoMC5_alt7 --spreadNH 5.0 $* &
+  mmLassoMC5_alt9 --spreadNH 5.0 $*
+  mmLassoMC5_alt5 --spreadNH 10.0 $* &
+  mmLassoMC5_alt7 --spreadNH 10.0 $* &
+  mmLassoMC5_alt9 --spreadNH 10.0 $*
+  mmLassoMC5_alt5 --spreadNH 25.0 $* &
+  mmLassoMC5_alt7 --spreadNH 25.0 $* &
+  mmLassoMC5_alt9 --spreadNH 25.0 $*
 }
+
+
+# find . | grep plot_x90_solve.pdf > thelist
+# echo "thelist.pdf" >> thelist
+# cat thelist | xargs pdfunite
+# evince thelist.pdf
