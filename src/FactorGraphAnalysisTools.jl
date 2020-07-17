5367d0a07067094aed133b09ee9bc792399058de
dehann <dehann.fourie@gmail.com>
2020-07-17 01:54:10 -0400

    suppliment deconv

--- src/FactorGraphAnalysisTools.jl
+++ src/FactorGraphAnalysisTools.jl
@@ -1,4 +1,6 @@
-# Graph analysis tools
+# Graph analysis tools and support calculation tools
+
+import IncrementalInference: selectFactorType, buildFactorDefault
 
 
 function rangeErrMaxPoint2(fgl1::AbstractDFG, id1, fgl2::AbstractDFG ,id2)
@@ -30,3 +32,24 @@ function rangeCompAllPoses(
   end
   return ranges
 end
+
+
+
+
+
+function selectFactorType(T1::Type{<:InferenceType}, T2::Type{<:InferenceType})
+  # initial hacky version
+  if T1 == Pose2 && T2 == Pose2
+    return Pose2Pose2
+  elseif T1 == Pose2 && T2 == Point2
+    return Pose2Point2BearingRange
+  elseif T1 == Point2 && T2 == Point2
+    return Point2Point2
+  else
+    error("dont know which Factor type to select between $T1 and $T2")
+  end
+end
+
+buildFactorDefault(::Type{Pose2Pose2}) = Pose2Pose2(MvNormal(zeros(3), diagm([0.01;0.01;0.01])))
+buildFactorDefault(::Type{Point2Point2}) = Point2Point2(MvNormal(zeros(2), diagm([0.01;0.01])))
+buildFactorDefault(::Type{Pose2Point2BearingRange}) = Pose2Point2BearingRange(Normal(0,0.1),Normal(1,0.1))
