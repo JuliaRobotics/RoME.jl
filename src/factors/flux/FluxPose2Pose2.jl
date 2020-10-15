6255cffd3c5d63a59b21ae908549bbf871c97ba7
dehann <dehann.fourie@gmail.com>
2020-10-15 16:23:04 -0400

    standardized factor

--- src/factors/flux/FluxPose2Pose2.jl
+++ src/factors/flux/FluxPose2Pose2.jl
@@ -3,33 +3,21 @@
 import Base: convert
 import IncrementalInference: getSample
 
-struct MixtureFlux{N,F<:FunctorInferenceType,S,T}
-  mixture::Mixture{N,F,S,T}
-  # special keyword field name used to invoke 'specialSampler' logic
-  specialSampler::Function 
-end
-
-const _IIFListTypes = Union{<:AbstractVector, <:Tuple, <:NTuple}
-
-function MixtureFlux( F_::FunctorInferenceType, 
-                      compList::_IIFListTypes, 
-                      diversity::Union{<:AbstractVector, <: NTuple}) 
-  #
-  mix = Mixuture(F_, )
-  MixtureFlux{N,F,S,T}()
-end
-
-MixtureFlux(::Type{F}, w...;kw...) where F <: FunctorInferenceType = MixtureFlux(F(LA.I), w...;kw...)
+export MixtureFluxPose2Pose2. PackedMixtureFluxPose2Pose2
 
 
-struct MixtureFluxPose2Pose2{N,S,T} <: AbstractRelativeRoots
-  mixture::Mixture{N,F,S,T}
+struct MixtureFluxPose2Pose2{F <: FunctorInferenceType} <: AbstractRelativeRoots
+  Zij::F
   # delta time between variables
   DT::Ref{Float64}
+  specialSampler::Function
 end
 
-# const MixtureFluxPose2Pose2{S,T} = MixtureFluxPose2Pose2{2,S,T}
-
+mutable struct PackedMixtureFluxPose2Pose2
+  packedMixture::PackedMixture
+  DT::Float64
+  specialSampler::String
+end
 
 function sampleMixtureFluxPose2Pose2( nfb::MixtureFluxPose2Pose2,
                                       N::Int,
@@ -47,18 +35,83 @@ end
 
 
 
-function MixtureFluxPose2Pose2(fluxmodels::AbstractVector,
-                                data::AbstractArray;
-                                naive::SamplableBelief=MvNormal(zeros(3),diagm(ones(3))),
+function MixtureFluxPose2Pose2( fluxmodels::AbstractVector,
+                                inputDims::NTuple,
+                                data::AbstractArray,
+                                outputDims::NTuple;
+                                otherComponents::AbstractVector{<:SamplableBelief}=[MvNormal(zeros(3),diagm(ones(3)));],
                                 diversity=[0.5;0.5],
                                 DT::Real=0,
-                                shuffle::Bool=true  )
+                                shuffle::Bool=true,
+                                serializeHollow::Bool=false  )
   #
-  mix = Mixture(Pose2Pose2, )
+  mix = MixtureFluxModels(Pose2Pose2, 
+                          fluxmodels, 
+                          inputDims, 
+                          data, 
+                          outputDims, 
+                          otherComponents, 
+                          diversity, 
+                          shuffle=shuffle, 
+                          serializeHollow=serializeHollow)
+  #
+  MixtureFluxPose2Pose2(mix, DT, sampleMixtureFluxPose2Pose2)
 end
 
 
 
+
+function Base.convert(::Union{Type{<:PackedInferenceType},Type{<:PackedMixtureFluxPose2Pose2}},
+                      obj::MixtureFluxPose2Pose2 )
+  #
+  toFnc = typeof(obj.specialSampler)
+  fncName = string(toFnc.name.module)*"."*string(toFnc.name.name)
+  PackedMixtureFluxPose2Pose2(convert(PackedMixture,obj.Zij),
+                              obj.DT[],
+                              fncName)
+end
+
+function Base.convert(::Union{Type{<:FunctorInferenceType},Type{<:MixtureFluxPose2Pose2}},
+                      obj::PackedMixtureFluxPose2Pose2 )
+  #
+  sFnc = split(obj.specialSampler, '.') .|> Symbol
+  specFnc = getfield(getfield(Main, sFnc[1]), sFnc[2])
+  PackedMixtureFluxPose2Pose2(convert(Mixture,obj.packedMixture),
+                                      obj.DT,
+                                      specFnc)
+end
+
+
+
+
+# struct MixtureFlux{N,F<:FunctorInferenceType,S,T}
+#   mixture::Mixture{N,F,S,T}
+#   # special keyword field name used to invoke 'specialSampler' logic
+#   specialSampler::Function 
+# end
+
+# # to be overrided by user via more specialized dispatch
+# # FIXME as part of 
+# function sampleMixtureFlux( nfb::MixtureFluxPose2Pose2,
+#                             N::Int,
+#                             fmd::FactorMetadata,
+#                             legacy... )
+#   #
+#   getSample(nfb.mixture, N)
+# end
+
+# # const _IIFListTypes = Union{<:AbstractVector, <:Tuple, <:NTuple}
+
+# function MixtureFlux( F_::FunctorInferenceType, 
+#                       compList::_IIFListTypes, 
+#                       diversity::Union{<:AbstractVector, <:NTuple, <:DiscreteNonParametric}) 
+#   #
+#   mix = Mixture(F_, compList, diversity)
+#   _populateMixture(mix{N,F,S,T}) where {N,F,S,T} = MixtureFlux{N,F,S,T}(mix, sampleMixtureFlux)
+#   return _populateMixture(mix)
+# end
+
+# MixtureFlux(::Type{F}, w...;kw...) where F <: FunctorInferenceType = MixtureFlux(F(LA.I), w...;kw...)
 
 
 
