e6f4ad9007d1f9ab0c29055c7f2db2f7bfa371e1
dehann <dehann.fourie@gmail.com>
2020-10-27 16:26:35 -0400

    last updates

--- examples/Hexagonal2D_BearingOnly_SLAM.jl
+++ examples/Hexagonal2D_BearingOnly_SLAM.jl
@@ -1,9 +1,12 @@
-# add more julia processes
+# import packages
+using RoME, Distributions
+
+# [OPTIONAL] add more julia processes to speed up inference
 using Distributed
 nprocs() < 3 ? addprocs(4-nprocs()) : nothing
 
-# tell Julia that you want to use these modules/namespaces
-using RoME, Distributions
+@everywhere using RoME
+
 
 # start with an empty factor graph object
 fg = initfg()
@@ -37,6 +40,7 @@ pl = plotSLAM2D(fg, contour=false)
 # For scripting use-cases you can export the image
 pl |> Gadfly.PDF("/tmp/test1.pdf", 20cm, 10cm)  # or PNG(...)
 
+
 # Add a landmark l1
 addVariable!(fg, :l1, Point2, tags=[:LANDMARK;])
 
@@ -45,13 +49,14 @@ bear2 = atan(10,10) - pi/3
 
 
 # Add landmarks with Bearing range measurements at x0, x6, x12, x18, x24, x30...
-vars = ls(fg)[1] # Get variables
+# Get variables
+vars = ls(fg, r"x") |> sortDFG
 for xIndex in 1:6:length(vars)
-    info("Creating factor between $(vars[xIndex]) and l1...")
+    @info("Creating factor between $(vars[xIndex]) and l1...")
     p2br = Pose2Point2Bearing(Normal(bear1,0.05))
-    addFactor!(fg, [@show vars[xIndex]; :l1], p2br)
+    addFactor!(fg, [vars[xIndex]; :l1], p2br)
     p2br = Pose2Point2Bearing(Normal(bear2,0.05))
-    addFactor!(fg, [@show vars[xIndex+1]; :l1], p2br)
+    addFactor!(fg, [vars[xIndex+1]; :l1], p2br)
 end
 
 # plotKDE(fg, [:x0;:x6;:x12], dims=[1;2])
@@ -65,7 +70,7 @@ ensureAllInitialized!(fg)
 pl = plotSLAM2D(fg, contour=false)
 pl |> Gadfly.PDF("/tmp/test2.pdf", 20cm, 10cm)  # or PNG(...)
 
-# solve
+# solve, at time of writing compute time should less than 45s 
 solveTree!(fg);
 
 # redraw to see that additional information aids in refining the results
