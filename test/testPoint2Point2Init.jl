1e0b2f46ec6e1778f65c71f796c1f5aae78ae316
dehann <dehann.fourie@gmail.com>
2021-02-16 01:02:30 -0500

    still need more inflation

--- test/testPoint2Point2Init.jl
+++ test/testPoint2Point2Init.jl
@@ -3,13 +3,15 @@ using RoME
 
 using Test
 
-
+##
 
 @testset "test tree based autoinit on small bimodal point2 trilateration..." begin
 
+##
 
 N=100
 fg = initfg()
+getSolverParams(fg).inflation=250.0
 
 addVariable!(fg, :x0, Point2, N=N)
 addFactor!(fg, [:x0], PriorPoint2(MvNormal([100.0;0], Matrix{Float64}(LinearAlgebra.I, 2,2))), graphinit=false)
@@ -21,28 +23,27 @@ addVariable!(fg, :l1, Point2, N=N)
 addFactor!(fg, [:x0;:l1], Point2Point2Range(Normal(100.0, 1.0)), graphinit=false)
 addFactor!(fg, [:x1;:l1], Point2Point2Range(Normal(100.0, 1.0)), graphinit=false)
 
-
-# tree = wipeBuildNewTree!(fg)
-# drawTree(tree, filepath="/tmp/caesar/bt.pdf", show=true)
-
-# eo = getEliminationOrder(fg, ordering=:qr)
-# eo = [1;3;5]
-# tree = buildTreeFromOrdering!(fg,eo)
+##
 
 tree, smt, hist = solveTree!(fg)
 
-# cliq = tree.cliques[2]
-# clst = cliqInitSolveUpByStateMachine!(fg, tree, cliq, drawtree=false, limititers=15 )
-
-# cliq = tree.cliques[1]
-# clst = cliqInitSolveUpByStateMachine!(fg, tree, cliq, drawtree=false, limititers=15 )
-
+##
 
 # using RoMEPlotting
-#
-# plotKDE(fg, [:x0;:x1]) |> SVG("/tmp/test.svg") || @async run(`eog /tmp/test.svg`)
-# plotKDE(fg, :l1) |> SVG("/tmp/test.svg") || @async run(`eog /tmp/test.svg`)
+# Gadfly.set_default_plot_size(25cm,20cm)
 
+# plotKDE(fg, [:x0;:x1]) # |> SVG("/tmp/test.svg") || @async run(`eog /tmp/test.svg`)
+# plotKDE(fg, :l1) # |> SVG("/tmp/test.svg") || @async run(`eog /tmp/test.svg`)
+
+##
+
+IIF._getCCW(fg, :x0l1f1).inflation = 1000.0
+pts = approxConv(fg, :x0l1f1, :l1)
+
+# plotKDE(manikde!(pts, Point2))
+
+
+##
 
 @test 10 < sum( 80 .< getVal(fg, :l1)[1,:] .< 120 )
 @test 10 < sum( -20 .< getVal(fg, :l1)[2,:] .< 20 )
@@ -79,3 +80,5 @@ tree, smt, hist = solveTree!(fg)
 
 
 end
+
+##
