2136a42aac28b03f07a44a58d98d0f1698ed6cba
dehann <dehann.fourie@gmail.com>
2021-08-12 20:27:15 -0400

    locality and docs for generate Beehive

--- src/canonical/GenerateBeehive.jl
+++ src/canonical/GenerateBeehive.jl
@@ -3,6 +3,16 @@
 export generateCanonicalFG_Beehive!
 
 
+"""
+    $SIGNATURES
+
+Pretend a bee is walking in a hive where each step (pose) follows one edge of an imaginary honeycomb lattice, 
+and at after each step a new direction left or right is stochastically chosen and the process repeats.
+
+Notes
+- The keyword `locality::Integer=3` is a positive integer value, where higher numbers imply direction decisions are more sticky for multiple steps.
+- Use keyword callback function `postpose_cb = (fg, lastpose) -> ...` to hook in your own features right after each new pose step.
+"""
 function generateCanonicalFG_Beehive!(poseCountTarget::Int=10;
                                       graphinit::Bool = true,
                                       dfg::AbstractDFG = LightDFG{SolverParams}(solverParams=SolverParams(graphinit=graphinit)),
@@ -15,7 +25,8 @@ function generateCanonicalFG_Beehive!(poseCountTarget::Int=10;
                                       pose0::Symbol=Symbol(match(r"[a-zA-Z_]+", poseRegex.pattern).match, 0),
                                       yaw0::Real = ([0.0;-2pi/3;2pi/3])[rand(1:3)],
                                       μ0::AbstractVector{<:Real} = [0;0;yaw0],                                  
-                                      postpose_cb::Function=(fg_,latestpose)->()     )
+                                      postpose_cb::Function=(fg_,latestpose)->(),
+                                      locality::Integer=2     )
   #
 
   # does anything exist in the graph yet
@@ -38,8 +49,10 @@ function generateCanonicalFG_Beehive!(poseCountTarget::Int=10;
   end
 
   # keep adding poses until the target number is reached
+  direction = rand(1:2) === 1 ? :left : :right
   while posecount < poseCountTarget
-    direction = rand(1:2) == 1 ? :left : :right
+    #change or keep direction according to location keyword
+    direction = rand(0:locality) !== 0 ? direction : (direction == :left ? :right : :left)
     posecount = _offsetHexLeg(dfg, posecount, direction=direction, graphinit=graphinit, 
                               landmarkSolvable=landmarkSolvable, poseCountTarget=poseCountTarget, 
                               postpose_cb=postpose_cb)
