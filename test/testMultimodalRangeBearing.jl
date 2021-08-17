57ff6fed7087d5ad12ea29fb5f059144194068ee
Johannes Terblanche <Affie@users.noreply.github.com>
2021-08-17 22:40:26 +0200

    small NorthSouthPartial getSample fix

--- test/testMultimodalRangeBearing.jl
+++ test/testMultimodalRangeBearing.jl
@@ -11,7 +11,7 @@ end
 
 NorthSouthPartial(Z::D) where {D <: IIF.SamplableBelief} = NorthSouthPartial(Z, (2,))
 
-getSample(cfo::CalcFactor{<:NorthSouthPartial}, N::Int=1) = (rand(cfo.factor.Z, N),)
+getSample(cfo::CalcFactor{<:NorthSouthPartial}, N::Int=1) = ([[rand(cfo.factor.Z)] for _=1:N],)
 
 ##
 
