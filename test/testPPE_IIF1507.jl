4bdd00d6b57cddb8b149c0ffe3d40a7377b6f05f
dehann <dehann.fourie@gmail.com>
2022-03-28 14:10:31 -0700

    better comment,
    Test failure until resolved JuliaRobotics/IncrementalInference.jl#1492

--- test/testPPE_IIF1507.jl
+++ test/testPPE_IIF1507.jl
@@ -5,7 +5,7 @@ using Test
 
 ##
 
-@testset "Test PPE not being populated by solve, IIF 1507" begin
+@testset "Wrong factor type behavior, IIF 1507" begin
 ##
 
 fg = initfg()
@@ -19,6 +19,9 @@ addFactor!(fg, [:x0], PriorPose2( MvNormal(zeros(3), diagm([0.1, 0.1, 0.1]))  ) 
 addFactor!(fg, [:x0; :x1], Pose2Pose2( MvNormal([1, 1, pi / 3], diagm([0.1, 0.1, 0.1])) ) )
 addFactor!(fg, [:x1; :x2], Pose2Pose2( MvNormal([1, 1, pi / 3], diagm([0.1, 0.1, 0.1])) ) )
 addFactor!(fg, [:l0], PriorPoint2( MvNormal([5, 0], diagm([2, 2])) ) )
+
+# purposefully send "wrong" factor type
+# see discussion: https://github.com/JuliaRobotics/IncrementalInference.jl/issues/1507#issuecomment-1081087381
 addFactor!(fg, [:x0;:l0], Point2Point2Range(Normal(5, 1)) )
 
 
