5ec685fc3767c796202c62f6e03e1e611c0d1dd6
Johannes Terblanche <6612981+Affie@users.noreply.github.com>
2022-05-15 17:24:05 +0200

    Update Pose2D.jl
    use \\mathrm{vee}

--- src/factors/Pose2D.jl
+++ src/factors/Pose2D.jl
@@ -10,14 +10,14 @@ Calcuated as:
 \\begin{aligned}
 \\hat{q}=\\exp_pX_m\\\\
 X = \\log_q \\hat{q}\\\\
-X^i = vee(q, X)
+X^i = \\mathrm{vee}(q, X)
 \\end{aligned}
 ```
 with:
 ``\\mathcal M= \\mathrm{SE}(2)`` Special Euclidean group\\
 ``p`` and ``q`` ``\\in \\mathcal M`` the two Pose2 points\\
 the measurement vector ``X_m \\in T_p \\mathcal M``\\
-and the error vector ``X \\in T_p \\mathcal M``\\
+and the error vector ``X \\in T_q \\mathcal M``\\
 ``X^i`` coordinates of ``X``
 
 DevNotes
