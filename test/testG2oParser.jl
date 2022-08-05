308e38c08c884464655104720295b821bea3e09d
Johannes Terblanche <Affie@users.noreply.github.com>
2022-08-05 22:14:20 +0200

    Restore g2oParser

--- test/testG2oParser.jl
+++ test/testG2oParser.jl
@@ -2,9 +2,11 @@ using Test
 using RoME
 
 @testset "Test g2o file parsing." begin
-
-
-  instructions = importG2o("octagon.g2o")
+  
+  
+  file = @__DIR__()*"/octagon.g2o"
+  instructions = importG2o(file)
+  # instructions = importG2o("octagon.g2o")
   # Choose random fields to test.
   @test instructions[1][1] == "EDGE_SE2"
   @test instructions[7][1] == "EDGE_SE2"
@@ -23,14 +25,15 @@ end
 
 @testset "Test g2o file parsing." begin
 
-  reflines = ["LANDMARK 0 1 0.0 20.0 3.162277660168379 0.0 1.0";
-              "EDGE_SE2 0 2 10.0 0.0 1.0471975511965976 10.0 10.0 0.0 0.0 10.0 0.0 0.0 0.0";
-              "EDGE_SE2 2 3 10.0 0.0 1.0471975511965976 10.0 10.0 0.0 0.0 10.0 0.0 0.0 0.0";
+  reflines = [
+              "EDGE_SE2 0 1 10.0 0.0 1.0471975511965976 10.0 10.0 0.0 0.0 10.0 0.0 0.0 0.0";
+              "LANDMARK 0 2 0.0 20.0 3.162277660168379 0.0 1.0";
+              "EDGE_SE2 1 3 10.0 0.0 1.0471975511965976 10.0 10.0 0.0 0.0 10.0 0.0 0.0 0.0";
               "EDGE_SE2 3 4 10.0 0.0 1.0471975511965976 10.0 10.0 0.0 0.0 10.0 0.0 0.0 0.0";
               "EDGE_SE2 4 5 10.0 0.0 1.0471975511965976 10.0 10.0 0.0 0.0 10.0 0.0 0.0 0.0";
               "EDGE_SE2 5 6 10.0 0.0 1.0471975511965976 10.0 10.0 0.0 0.0 10.0 0.0 0.0 0.0";
               "EDGE_SE2 6 7 10.0 0.0 1.0471975511965976 10.0 10.0 0.0 0.0 10.0 0.0 0.0 0.0";
-              "LANDMARK 7 1 0.0 20.0 3.162277660168379 0.0 1.0"]
+              "LANDMARK 7 2 0.0 20.0 3.162277660168379 0.0 1.0"]
 
 
   # test file generation
