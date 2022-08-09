cb91864c51f32e5c203dfe767c1659eb0953bba3
Johannes Terblanche <Affie@users.noreply.github.com>
2022-08-09 16:14:02 +0200

    Fix BearingRange Factor

--- src/factors/BearingRange2D.jl
+++ src/factors/BearingRange2D.jl
@@ -36,61 +36,22 @@ function IIF.getMeasurementParametric(s::Pose2Point2BearingRange{<:Normal, <:Nor
   return meas, iΣ
 end
 
-function (cfo::CalcFactor{<:Pose2Point2BearingRange})(meas, xi, lm)
-  SE2 = SpecialEuclidean(2)
-  #FIXME fix this factor to work correctly on manifolds
-  if false
+function (cfo::CalcFactor{<:Pose2Point2BearingRange})(measX, p, l)
+  #
+  M = getManifold(cfo.factor)
 
-  Mt = SE2.manifold.manifolds[1]
-  Mr = SE2.manifold.manifolds[2]
+  # wl = l
+  # wTp = p
+  # pl = pTw*wl
+  pl  =  transpose(p.x[2]) * (l - p.x[1])
 
-  # compose rotations
-  rRi = xi.parts[2]
-  rRo = retract(Mr, rRi, meas.parts[1])
-
-  # new SE2 objects containing the rotation
-  rTo = ArrayPartition(xi.parts[1], rRo)
-  oTl = ArrayPartition([meas.parts[2];0], identity_element(Mr, rRi))
-
-  # prediction landmark
-  rTo = Manifolds.compose(SE2, rTo, oTl)
-
-  # cartesian difference in predicted and estimated landmark
-  δ_l = Manifolds.compose(Mt, inv(Mt, rTo.parts[1]), lm)
-
-  # find the residuals as though in polar coordinates
-  δθ = atan(δ_l[2], δ_l[1])
-  δr = norm(δ_l)
+  mθ,mr = vee(M, Manifolds.Identity(M), measX) 
+  δθ = mθ - atan(pl[2], pl[1])
+  δr = mr - norm(pl)
 
   return [δθ; δr]
+end
 
-  # FIXME: this is close to the old BR that worked, use to ensure tests are working
-  else
-    Xi = vee(SE2, xi, log(SE2, identity_element(SE2, xi), xi))
-
-    Mmeas = getManifold(cfo.factor)
-    Xmeas = vee(Mmeas, Manifolds.Identity(Mmeas), meas)
-    # 1-bearing
-    # 2-range
-    # world frame
-    meas_θ = Xmeas[1]
-    meas_r = Xmeas[2]
-
-    θ = meas_θ + Xi[3]
-    mx = meas_r*cos(θ)
-    my = meas_r*sin(θ)
-  
-    ex = lm[1] - (mx + Xi[1])
-    ey = lm[2] - (my + Xi[2])
-  
-    # res = [eθ, er]
-    eθ = atan((my + Xi[2]), (mx + Xi[1])) - atan(lm[2], lm[1])  # eθ
-    er= sqrt(ex^2 + ey^2) # some wasted computation here       # er 
-  
-    # return hat(facM, ProductRepr([1. 0;0 1], [0.]), [eθ, er])
-    return [eθ, er]
-  end
-end  
 # quick check
 # pose = (0,0,0),  bear = 0.0,  range = 10.0   ==>  lm = (10,0)
 # pose = (0,0,0),  bear = pi/2,  range = 10.0   ==>  lm = (0,10)
@@ -98,17 +59,6 @@ end
 # pose = (0,0,pi/2),  bear = pi/2,  range = 10.0   ==>  lm = (-10,0)
 
 
-# function (cfo::CalcFactor{<:Pose2Point2BearingRange})(measX, p, q)
-#   #
-#   M = SpecialEuclidean(2)
-#   q_SE = ProductRepr(q, identity_element(SpecialOrthogonal(2), p.parts[2]))
-
-#   X_se2 = log(M, identity_element(M, p), compose(M, inv(M, p), q_SE))
-#   X = X_se2.parts[1]
-#   # NOTE this: `X̂ = log(M, p, q_SE)` wrong for what we want
-#   #go to tangent vector
-#   return measX - X 
-# end
 
 # Support for database based solving
 
