d671d46db9638e8d916a2eda25daf5e9b4174299
dehann <dehann.fourie@gmail.com>
2022-09-11 22:00:14 -0700

    expand precompile

--- src/AdditionalUtils.jl
+++ src/AdditionalUtils.jl
@@ -8,7 +8,7 @@ Load and solve a canonical or user factor graph to warm up---precompile---severa
 function warmUpSolverJIT(;fg::AbstractDFG=generateGraph_Hexagonal(),
                           drawtree::Bool=false )
   #
-
+  
   fcts = ls(fg, :x0)
   fcts = ls(fg)
   fcts = lsf(fg, :x0f1)
@@ -17,6 +17,21 @@ function warmUpSolverJIT(;fg::AbstractDFG=generateGraph_Hexagonal(),
   # getSolverParams(fg).drawtree = drawtree
   initAll!(fg)
   tree = solveTree!(fg)
+  
+  IIF.initParametricFrom!(fg, :default)
+  IIF.solveGraphParametric!(fg)
+  
+  fg = generateGraph_Hexagonal(graphinit=false)
+  initAll!(fg)
+  
+  fg = initfg()
+  addVariable!(fg, :x0, Pose3)
+  addVariable!(fg, :x1, Pose3)
+  addFactor!(fg, [:x0], PriorPose3(MvNormal(zeros(6), diagm(ones(6)))))
+  addFactor!(fg, [:x0;:x1], Pose3Pose3(MvNormal(zeros(6))))
+  
+  solveGraph!(fg)
+
   nothing
 end
 
