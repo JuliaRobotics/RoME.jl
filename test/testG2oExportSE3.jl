efff4eb383f553f315649d4a3976fa6f1899b905
dehann <dehann.fourie@gmail.com>
2023-03-02 21:47:57 -0800

    fix test g2o export

--- test/testG2oExportSE3.jl
+++ test/testG2oExportSE3.jl
@@ -10,6 +10,14 @@ using Test
 @testset "Test g2o SE3 export" begin
 ##
 
+# fg = initfg()
+# addVariable!.(fg, [:x0;:x1;:x2;:x3], Pose3)
+# addFactor!(fg, [:x0], PriorPose3(MvNormal(zeros(6),diagm(0.1*ones(6)))))
+# addFactor!(fg, [:x0;:x1], Pose3Pose3(MvNormal([1;0;0;0;0;0.],diagm(0.1*ones(6)))); graphinit=false)
+# addFactor!(fg, [:x1;:x2], Pose3Pose3(MvNormal([1;0;0;0;0;0.],diagm(0.1*ones(6)))); graphinit=false)
+# addFactor!(fg, [:x2;:x3], Pose3Pose3(MvNormal([1;0;0;0;0;0.],diagm(0.1*ones(6)))); graphinit=false)
+# saveDFG(joinpath(@__DIR__,"testdata","g2otest.tar.gz"), fg)
+
 fg = loadDFG(joinpath(@__DIR__,"testdata","g2otest.tar.gz"))
 
 setPPE!.(fg, ls(fg), :parametric)
