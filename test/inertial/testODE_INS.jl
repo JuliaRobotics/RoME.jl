2a2ba0b48f9e936836b070e94847d633c03ba9a6
dehann <dehann.fourie@gmail.com>
2023-11-13 23:51:07 -0800

    consol inertial factors with genField_Inertial

--- test/inertial/testODE_INS.jl
+++ test/inertial/testODE_INS.jl
@@ -1,93 +1,34 @@
 # Basic test of DERelative
 
 
-using Test
 using DifferentialEquations
-using RoME
-import RoME: imuKinematic!
-
 using Interpolations
-using IncrementalInference
 using Dates
 using Statistics
 using TensorCast
 using StaticArrays
 using Manifolds
-import Base: convert
+
+using IncrementalInference
+using RoME
+# import RoME: imuKinematic!
+# import Base: convert
+
+using Test
 
 ##
 
 @testset "DERelative INS Kinematic tests" begin
 ##
 
-# a user specified ODE in standard form
-# inplace `xdot = f(x, u, t)`
-# if linear, `xdot = F*x(t) + G*u(t)`
-function insKinematic!(dstate, state, u, t)
-  # x is a VelPose3 point (assumed ArrayPartition)
-  # u is IMU input (assumed [rate; accel])
-  Mt = TranslationGroup(3)
-  Mr = SpecialOrthogonal(3)
-  # convention
-  # b is real time body
-  # b1 is one discete timestep in the past, equivalent to `r_Sk = r_Sk1 + r_dSk := r_S{k-1} + r_dSk`
-
-  # Using robotics frame fwd-std-dwn <==> North-East-Down
-  # ODE cross check taken from Farrell 2008, section 11.2.1, p.388
-  # NOTE, Farrell 2008 has gravity logic flipped in some of his equations.
-  # WE TAKE gravity as up is positive (think a pendulum hanging back during acceleration)
-  # expected gravity in FSD frame (akin to NED).  This is a model of gravity we expect to measure.
-  i_G = [0; 0; -9.81] 
-
-  # attitude computer
-  w_R_b = state.x[2] # Rotation element
-  i_R_b = w_R_b
-  # assume body-frame := imu-frame
-  b_Ωbi = hat(Mr, Identity(Mr), u[].gyro(t)) # so(3): skew symmetric Lie algebra element
-  # assume perfect measurement, i.e. `i` here means measured against native inertial (no coriolis, transport rate, error corrections)
-  i_Ṙ_b = i_R_b * b_Ωbi
-  # assume world-frame := inertial-frame
-  w_Ṙ_b = i_Ṙ_b
-  # tie back to the ODE solver
-
-  dstate.x[2] .= w_Ṙ_b
-  # Note closed form post integration result (remember exp is solution to first order diff equation)
-  # w_R_b = exp(Mr, w_R_b1, b_Ωbi)
-  
-  # measured inertial acceleration
-  b_Abi = u[].accel(t) # already a tangent vector
-  # inertial (i.e. world) velocity-dot (accel) by compensating (i.e. removing) expected gravity measurement
-  i_V̇ = i_R_b * b_Abi - i_G
-  # assume world is inertial frame
-  w_V̇ = i_V̇
-  dstate.x[3] .= w_V̇ # velocity state
-  
-  # position-dot (velocity)
-  w_V = state.x[3]
-  i_V = w_V
-  i_Ṗ = i_V
-  w_Ṗ = i_Ṗ
-  dstate.x[1] .= w_Ṗ
-  
-  # TODO add biases, see RoME.InertialPose
-  # state[4] := gyro bias
-  # state[5] := acce bias
-
-  nothing
-end
-
 dt = 0.01
 N = 101
-gyros = [[0.01, 0.0, 0.0] for _ = 1:N]
-a0 = [0, 0, -9.81]
-accels = [a0]
-w_R_b = [1. 0 0; 0 1 0; 0 0 1]
-M = SpecialOrthogonal(3)
-for g in gyros[1:end-1]
-  X = hat(M, Identity(M), g)
-  exp!(M, w_R_b, w_R_b, X*dt)
-  push!(accels, w_R_b' * a0)
-end
+imu = RoME.generateField_InertialMeasurement_RateZ(;
+  dt,
+  N,
+  rate = [0.01, 0, 0],
+  accel0 = [0, 0, -9.81], #WHY NEGATIVE, LEGACY, FIX TO +!  DISCREP BETWEEN insKinematic and imuKinematic
+)
 
 gyros_t = linear_interpolation(range(0; step=dt, length=N), gyros)
 accels_t = linear_interpolation(range(0; step=dt, length=N), accels)
@@ -99,7 +40,7 @@ p.accel(0.9)
 u0 = ArrayPartition([0.0,0,0], Matrix(getPointIdentity(SpecialOrthogonal(3))), [0.,0,0])
 tspan = (0.0, 1.0)
 
-prob = ODEProblem(insKinematic!, u0, tspan, Ref(p))
+prob = ODEProblem(RoME.insKinematic!, u0, tspan, Ref(p))
 
 sol = solve(prob)
 last(sol)
@@ -116,52 +57,12 @@ end
 @testset "DERelative IMU Kinematic tests" begin
 ##
 
-# ## TODO consolidate inside module as RoME.imuKinematic
-# function imuKinematic!(du, u, p, t)
-#   # p is IMU input (assumed [.gyro; .accel])
-#   M = SpecialOrthogonal(3)
-
-#   R = u.x[1] # Rotation
-#   V = u.x[2] # Velocity 
-#   # P = u.x[3] # Position unused here
-#   # ω_b = u.x[4] # Gyroscope bias
-#   # A_b = u.x[5] # Accelerometer bias
-
-#   ω_m = p[].gyro(t)
-#   Ω = hat(M, Identity(M), ω_m)# + ω_b)
-#   Ṙ = R * Ω # d/dt R = d/dt exp(Ω*Δt) => Ṙ = exp(ΩΔt)*d(ΩΔt)/dt = exp(ΩΔt)*Ω
-
-#   A_m = p[].accel(t)
-#   V̇ = R * (A_m) # + A_b)
-#   Ṗ = V
-
-#   du.x[1] .= Ṙ
-#   du.x[2] .= V̇
-#   du.x[3] .= Ṗ
-
-#   nothing
-# end
-
-
 dt = 0.01
-# N = 1001
 N = 401
-# tspan = (0.0, dt*(N-1))
 
-gyros = [[0.0, 0.0, pi/2] for _ = 1:N]
-
-a0 = [0.0, 0, 0]
-accels = [a0]
-w_R_b = [1. 0 0; 0 1 0; 0 0 1]
-M = SpecialOrthogonal(3)
-
-# b_a = [0.1, 0, 0]
-b_a = [0.0, pi/2*10, 0]
-for g in gyros[1:end-1]
-  X = hat(M, Identity(M), g)
-  exp!(M, w_R_b, w_R_b, X*dt)
-  push!(accels, b_a .+ w_R_b * a0)
-end
+imu = RoME.generateField_InertialMeasurement_RateZ(;dt, N)
+gyros = imu.gyros
+accels = imu.accels
 
 ##
 
@@ -203,10 +104,8 @@ oder = DERelative(
   fg, 
   [:w_P0; :w_P1], 
   RotVelPos, 
-  imuKinematic!,
+  RoME.imuKinematic!,
   imuForce;
-  # state0=allocate(getPointIdentity(RotVelPos)),
-  # state1=allocate(getPointIdentity(RotVelPos)),
   dt, 
   problemType=ODEProblem 
 );
@@ -230,7 +129,7 @@ addFactor!( fg, [:w_P0; :w_P1], oder; graphinit=false );
 P1 = approxConvBelief(fg, :w_P0w_P1f1, :w_P1)
 
 
-# ## basic sample test
+## basic sample test
 
 # meas = sampleFactor(fg, :x0x1f1, 10)
 # @test size(meas[1][1],1) == 1
