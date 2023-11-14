085e7f085e02e893a4b114afc79363a08eb01e58
dehann <dehann.fourie@gmail.com>
2023-11-14 01:53:42 -0800

    consol test genField InertialDynamic

--- test/inertial/testInertialDynamic.jl
+++ test/inertial/testInertialDynamic.jl
@@ -13,32 +13,26 @@ using StaticArrays
 
 ## test factor with rotation around z axis and initial velocity up
 # DUPLICATED IN testIMUDeltaFactor.jl
-dt = 0.1
-σ_a = 1e-4#0.16e-3*9.81  # noise density m/s²/√Hz
-σ_ω = deg2rad(0.0001)  # noise density rad/√Hz
-gn = MvNormal(diagm(ones(3)*σ_ω^2 * 1/dt))
-an = MvNormal(diagm(ones(3)*σ_a^2 * 1/dt))
 
-# Σy  = diagm([ones(3)*σ_a^2; ones(3)*σ_ω^2])
-gyros = [SA[0, 0, 0.001] + rand(gn) for _ = 1:11]
-accels = [SA[0, 0, 9.81 - 1] + rand(an) for _ = 1:11]
+dt = 0.1
+N = 11
+
+σ_a = 1e-4 #0.16e-3*9.81  # noise density m/s²/√Hz
+σ_ω = deg2rad(0.0001)  # noise density rad/√Hz
+imu = RoME.generateField_InertialMeasurement_RateZ_noise(; dt, N, σ_a, σ_ω)
 
 tst = now(localzone())
-tsp = tst + Second(1)
+tsp = tst + Second(imu.tspan[2]-imu.tspan[1])
 tspan = (tst,tsp)
-
-# a_b = SA[0.,0,0]
-# ω_b = SA[0.,0,0]
 
 ##
 
 fac = RoME.InertialDynamic(
   tspan,
   dt,
-  accels,
-  gyros,
+  imu.accels,
+  imu.gyros,
 )
-
 
 ## build a basic factor graph
 
@@ -70,8 +64,14 @@ f1 = addFactor!(fg, [:w_P0; :w_P1], fac; graphinit=false)
 doautoinit!(fg, :w_P0)
 @test isInitialized(fg, :w_P0)
 
+try
+  P1 = approxConvBelief(fg, getLabel(f1), :w_P1)
+catch
+  @error "FIXME first approxConv on InertialDynamic failed!"
+end
 
 P1 = approxConvBelief(fg, getLabel(f1), :w_P1)
+
 
 
 ##
