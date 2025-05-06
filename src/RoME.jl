2d5d9487b1853c90f735ade0cf43ee82fc59714c
dehann <dehann.fourie@gmail.com>
2025-05-06 09:59:13 -0700

    suppress precompile workload

--- src/RoME.jl
+++ src/RoME.jl
@@ -142,9 +142,9 @@ include("Deprecated.jl")
 
 # manifold conversions required during transformation
 
-@compile_workload begin
-  # In here put "toy workloads" that exercise the code you want to precompile
-  warmUpSolverJIT()
-end
+# @compile_workload begin
+#   # In here put "toy workloads" that exercise the code you want to precompile
+#   warmUpSolverJIT()
+# end
 
 end
