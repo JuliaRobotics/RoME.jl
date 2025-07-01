1ce734292c2f965b810293b989a71a47765e1781
Johannes Terblanche <Affie@users.noreply.github.com>
2025-07-01 23:00:54 +0200

    missed exports

--- RoMETypes/src/RoMETypes.jl
+++ RoMETypes/src/RoMETypes.jl
@@ -28,7 +28,15 @@ export
     Pose2Pose2,
     PackedPose2Pose2,
     Pose3Pose3,
-    PackedPose3Pose3
+    PackedPose3Pose3,
+    PriorPose3,
+    PackedPriorPose3,    
+    Point3Point3,
+    PackedPoint3Point3,
+    Point2Point2,
+    PackedPoint2Point2,
+    PriorPose2,
+    PackedPriorPose2
 
 include("variables/VariableTypes.jl")
 include("factors/FactorTypes.jl")
