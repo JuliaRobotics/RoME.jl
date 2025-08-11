5a1ee0bae19a7d18e50643b294d0d700df955192
Johannes Terblanche <Affie@users.noreply.github.com>
2025-08-11 18:22:28 +0200

    Manually merge conflics in Pose2Pose2 and PriorPose2

--- src/factors/Poses.jl
+++ src/factors/Poses.jl
@@ -1,21 +1,4 @@
 
-#TODO deprecate, only use LieGroups
-@inline function _vee(
-    ::typeof(SpecialEuclidean(2; vectors = HybridTangentRepresentation())),
-    X::ArrayPartition{T, Tuple{SVector{2, T}, SMatrix{2, 2, T, 4}}},
-) where {T <: Real}
-    return SVector{3, T}(X.x[1][1], X.x[1][2], X.x[2][2])
-end
-
-#TODO deprecate, only use LieGroups
-@inline function _compose(
-    ::typeof(SpecialEuclidean(2; vectors = HybridTangentRepresentation())),
-    p::ArrayPartition{T, Tuple{SVector{2, T}, SMatrix{2, 2, T, 4}}},
-    q::ArrayPartition{T, Tuple{SVector{2, T}, SMatrix{2, 2, T, 4}}},
-) where {T <: Real}
-    return ArrayPartition(p.x[1] + p.x[2] * q.x[1], p.x[2] * q.x[2])
-end
-
 function (cf::CalcFactor{<:PriorPose2})(
     _m::AbstractArray{MT},
     _p::AbstractArray{PT},
@@ -26,62 +9,27 @@ function (cf::CalcFactor{<:PriorPose2})(
     return cf(m, p)
 end
 
-# TODO the log here looks wrong (for gradients), consider:
-# X = log(p⁻¹ ∘ m) 
-# X = log(M, ϵ, Manifolds.compose(M, inv(M, p), m))
 function (cf::CalcFactor{<:PriorPose2})(
     m::ArrayPartition{T, Tuple{SVector{2, T}, SMatrix{2, 2, T, 4}}},
     p::ArrayPartition{T, Tuple{SVector{2, T}, SMatrix{2, 2, T, 4}}},
 ) where {T <: Real}
-    M = getManifold(Pose2)
-    ϵ = getPointIdentity(M)
-    Xc = _vee(M, log(M, p, m))
-    # X = log(M, ϵ, Manifolds.compose(M, inv(M, p), m))
-    # Xc = vee(M, ϵ, X)
-    return Xc
+    M = getManifold(PriorPose2)
+    X = log(M, p, m) # Currently X ∈ TₚM, #TODO should it be TₘM?
+    return vee(M, p, X)
 end
 
 ## NOTE likely deprecated comparitors, see DFG compareFields, compareAll instead
 compare(a::PriorPose2, b::PriorPose2; tol::Float64 = 1e-10) = compareDensity(a.Z, b.Z)
 
-# Assumes X is a tangent vector
-function (cf::CalcFactor{<:Pose2Pose2})(
-    _X::AbstractArray{MT},
-    _p::AbstractArray{PT},
-    _q::AbstractArray{LT},
-) where {MT, PT, LT}
-    #TODO remove this convertions
-    # @warn "This warning should not be triggered after StaticArrays upgrade" maxlog=10
-    T = promote_type(MT, PT, LT)
-    X = convert(ArrayPartition{T, Tuple{SVector{2, T}, SMatrix{2, 2, T, 4}}}, _X)
-    p = convert(ArrayPartition{T, Tuple{SVector{2, T}, SMatrix{2, 2, T, 4}}}, _p)
-    q = convert(ArrayPartition{T, Tuple{SVector{2, T}, SMatrix{2, 2, T, 4}}}, _q)
-    return cf(X, p, q)
-end
-
-# function calcPose2Pose2(
-function (cf::CalcFactor{<:Pose2Pose2})(
-    X::ArrayPartition{XT, Tuple{SVector{2, XT}, SMatrix{2, 2, XT, 4}}},
-    p::ArrayPartition{T, Tuple{SVector{2, T}, SMatrix{2, 2, T, 4}}},
-    q::ArrayPartition{T, Tuple{SVector{2, T}, SMatrix{2, 2, T, 4}}},
-) where {XT <: Real, T <: Real}
-    M = getManifold(Pose2)
-    # ϵ0 = ArrayPartition(zeros(SVector{2,T}), SMatrix{2, 2, T}(I))
-    ϵ0 = getPointIdentity(M)
-
-    ϵX = exp(M, ϵ0, X)
-    # q̂ = Manifolds.compose(M, p, ϵX)    
-    q̂ = _compose(M, p, ϵX)
-    X_hat = log(M, q, q̂)#::ArrayPartition{T, Tuple{SVector{2, T}, SMatrix{2, 2, T, 4}}}
-    # Xc = vee(M, q, X_hat)
-    Xc = _vee(M, X_hat)#::SVector{3,T}
-    return Xc
+function (cf::CalcFactor{<:Pose2Pose2})(X, p, q)
+  # X ∈ TₚM, X̂ ∈ TₚM, p,q ∈ M
+  M = getManifold(Pose2)
+  X̂ = log(M, p, q)
+  return vee(M, p, X - X̂) # TODO check sign
 end
 
 # FIXME, rather have separate compareDensity functions
 compare(a::Pose2Pose2, b::Pose2Pose2; tol::Float64 = 1e-10) = compareDensity(a.Z, b.Z)
-
-#
 
 # regular prior for Pose3
 
@@ -94,26 +42,11 @@ end
 # Pose3Pose3 evaluation functions
 
 function (cf::CalcFactor{<:Pose3Pose3})(X, p::ArrayPartition{T}, q) where {T}
-    M = getManifold(Pose3)
-    # X: p_Xq̂
-    # p: w_H_p
-    # q: w_H_q
-    # ̂q: w_H_̂q = w_H_p * exp_0(p_Xq̂)
-    # w_H_̂q = Manifolds.compose(M, w_H_p, exp(M, getPointIdentity(M), p_Xq̂))
-
-    q̂ = Manifolds.compose(M, p, exp(M, getPointIdentity(M), X))
-
-    Xc::SVector{6, T} = get_coordinates(M, q, log(M, q, q̂), DefaultOrthogonalBasis())
+    M = getManifold(Pose3Pose3)
+    X̂ = log(M, p, q)
+    Xc::SVector{6, T} = vee(M, p, X - X̂)
     return Xc
 end
-
-# function (cf::CalcFactor{<:Pose3Pose3})(X, p, q)  
-#   M = cf.manifold # getManifold(Pose3)
-#   ϵX = exp(M, getPointIdentity(M), X)
-#   q̂ = ArrayPartition(p.x[2]*ϵX.x[1] + p.x[1], p.x[2]*ϵX.x[2])
-#   Xc = vee(M, q, log(M, q, q̂))
-#   return Xc
-# end
 
 ##
 #TODO is this manifold not SO3
